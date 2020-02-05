a27a346f29ba464785a5c0f3bc95dc8abd993027
Sirisak Lueangsaksri <sirisak.l@pomelofashion.com>
2020-02-05 11:57:18 +0700

    fix(packages): address sed is not edit in place

--- packages.sh
+++ packages.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 echo "Installing docker..."
-echo "References:"
-echo "  - https://www.docker.com/blog/happy-pi-day-docker-raspberry-pi/"
-echo "  - https://howchoo.com/g/nmrlzmq1ymn/how-to-install-docker-on-your-raspberry-pi"
 if test ! "$(command -v docker)"; then
+  echo "References:"
+  echo "  - https://www.docker.com/blog/happy-pi-day-docker-raspberry-pi/"
+  echo "  - https://howchoo.com/g/nmrlzmq1ymn/how-to-install-docker-on-your-raspberry-pi"
+  sleep 1
   curl -sSL get.docker.com | sh
   sudo usermod -aG docker $USER
 else
   echo "docker already installed"
+  sleep 1
 fi
 
 cd $HOME
 echo "Installing hstr..."
-echo "References:"
-echo "  - https://github.com/dvorka/hstr/blob/master/INSTALLATION.md#build-on-any-linux-distro"
 if test ! "$(command -v hstr)"; then
+  echo "References:"
+  echo "  - https://github.com/dvorka/hstr/blob/master/INSTALLATION.md#build-on-any-linux-distro"
+  sleep 1
   git clone https://github.com/dvorka/hstr
   cd ./hstr/build/tarball
   ./tarball-automake.sh
@@ -27,13 +30,15 @@ if test ! "$(command -v hstr)"; then
   rm -rf hstr
 else
   echo "hstr already installed"
+  sleep 1
 fi
 
 cd $HOME
 echo "Installing neovim..."
-echo "References:"
-echo "  - https://github.com/neovim/neovim/wiki/Building-Neovim#building"
 if test ! "$(command -v nvim)"; then
+  echo "References:"
+  echo "  - https://github.com/neovim/neovim/wiki/Building-Neovim#building"
+  sleep 1
   git clone https://github.com/neovim/neovim
   cd neovim
   make CMAKE_BUILD_TYPE=Release
@@ -42,27 +47,31 @@ if test ! "$(command -v nvim)"; then
   rm -rf neovim
 else
   echo "neovim already installed"
+  sleep 1
 fi
 
 cd $HOME
 echo "Installing cmus..."
-echo "References:"
-echo "  - https://cmus.github.io/#download"
 if test ! "$(command -v cmus)"; then
+  echo "References:"
+  echo "  - https://cmus.github.io/#download"
+  sleep 1
   git clone https://github.com/cmus/cmus .cmus
   cd .cmus
-  sed -e 's/-lm/-lm -latomic/g' Makefile
+  sed -i 's/-lm/-lm -latomic/g' Makefile
   ./configure prefix=$HOME/.cmus
   sudo make install
 else
   echo "cmus already installed"
+  sleep 1
 fi
 
 cd $HOME
 echo "Installing mosh..."
-echo "References:"
-echo "  - https://github.com/mobile-shell/mosh/issues/961#issuecomment-565741393"
 if test ! "$(command -v cmus)"; then
+  echo "References:"
+  echo "  - https://github.com/mobile-shell/mosh/issues/961#issuecomment-565741393"
+  sleep 1
   git clone https://github.com/mobile-shell/mosh
   cd mosh
   ./autogen.sh
@@ -73,8 +82,10 @@ if test ! "$(command -v cmus)"; then
   rm -rf mosh
 else
   echo "mosh already installed"
+  sleep 1
 fi
 
 cd $HOME
 echo "Installing oh-my-zsh..."
+sleep 1
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
