319fb2d49997c3a323a022a3f686d88302971070
Sirisak Lueangsaksri <spywhere@me.com>
2020-04-25 12:52:16 +0700

    refactor(script): only show Docker on non-linux setup

--- scripts/running.sh
+++ scripts/running.sh
@@ -1,18 +1,19 @@
-if command docker 2>/dev/null; then
-  docker_start=""
-  docker_info=$(docker info 2>&1)
-  if [ $? -eq 0 ]; then
-    docker_start=""
-  fi
-  if echo $docker_info | grep "refused"; then
-    docker_start=""
-  fi
+docker_start=""
 
-  if [ $docker_start -z ]; then
-    printf ""
-  else
-    printf "  %s" $docker_start
+if [ "$(uname)" = "Darwin" ]; then
+  if command docker 2>/dev/null; then
+    docker_info=$(docker info 2>&1)
+    if [ $? -eq 0 ]; then
+      docker_start=""
+    fi
+    if echo $docker_info | grep "refused"; then
+      docker_start=""
+    fi
   fi
-else
+fi
+
+if [ $docker_start -z ]; then
   printf ""
+else
+  printf "  %s" $docker_start
 fi
