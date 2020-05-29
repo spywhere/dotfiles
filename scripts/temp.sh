cd04e913bbd1148ddf211289fff746f1aafa890d
Sirisak Lueangsaksri <spywhere@me.com>
2020-05-30 01:06:56 +0700

    refactor(tmux): remove decimal in temperature widget

--- scripts/temp.sh
+++ scripts/temp.sh
@@ -1,5 +1,5 @@
 if command vcgencmd 2>/dev/null; then
-  echo $(vcgencmd measure_temp | sed -e "s/temp=//" -e "s/'C//")
+  echo $(vcgencmd measure_temp | sed -e "s/temp=//" -e "s/.0'C//")
 else
   echo ""
 fi
