d184c078c2d1d1b309a0897c4c9defb9a96b6da1
Sirisak Lueangsaksri <spywhere@me.com>
2020-10-06 15:45:23 +0700

    refactor(structure): adopting a new structure

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ if [ "$(uname)" = "Darwin" ]; then
     rm -rf "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
     # Remove the directory itself
     rm -rf "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
-    ln -s "$HOME/$DOTFILES_NAME/files/iterm2/DynamicProfiles" "$HOME/Library/Application Support/iTerm2"
+    ln -s "$HOME/$DOTFILES_NAME/supports/iterm2/DynamicProfiles" "$HOME/Library/Application Support/iTerm2"
   fi
 else
   echo "Setting up system..."
@@ -24,9 +24,9 @@ else
     echo "denyinterfaces usb0" | sudo tee -a /etc/dhcpcd.conf >/dev/null
     mkdir -p /etc/dnsmasq.d
     mkdir -p /etc/network/interfaces.d
-    cat $HOME/$DOTFILES_NAME/files/etc/dnsmasq.d/usb | sudo tee /etc/dnsmasq.d/usb >/dev/null
-    cat $HOME/$DOTFILES_NAME/files/etc/network/interfaces.d/usb0 | sudo tee /etc/network/interfaces.d/usb0 >/dev/null
-    cat $HOME/$DOTFILES_NAME/files/root/usb.sh | sudo tee /root/usb.sh >/dev/null
+    cat $HOME/$DOTFILES_NAME/supports/usbc/etc/dnsmasq.d/usb | sudo tee /etc/dnsmasq.d/usb >/dev/null
+    cat $HOME/$DOTFILES_NAME/supports/usbc/etc/network/interfaces.d/usb0 | sudo tee /etc/network/interfaces.d/usb0 >/dev/null
+    cat $HOME/$DOTFILES_NAME/supports/usbc/root/usb.sh | sudo tee /root/usb.sh >/dev/null
     sudo chmod 755 /root/usb.sh
     sudo sed -i $'s/exit 0$/sh \\/root\\/usb.sh\\\nexit 0/g' /etc/rc.local
   fi
