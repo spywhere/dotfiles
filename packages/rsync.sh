c01e3f5073e5efa02efde0c1448138450c596a0f
Sirisak Lueangsaksri <spywhere@me.com>
2020-12-06 17:20:54 +0700

    fix(installation): not working in quiet mode

--- packages/rsync.sh
+++ packages/rsync.sh
@@ -3,9 +3,9 @@
 set -e
 
 if
-  (! command -v print >/dev/null 2>&1) ||
-  ! $(print 3 a b >/dev/null 2>&1) ||
-  test "$(print 3 a b)" != "a  b";
+  (! command -v force_print >/dev/null 2>&1) ||
+  ! $(force_print 3 a b >/dev/null 2>&1) ||
+  test "$(force_print 3 a b)" != "a  b";
 then
   printf "Please run this script through \"install.sh\" instead"
   exit 1
