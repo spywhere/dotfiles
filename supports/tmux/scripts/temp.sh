8cf40787eb34ae59dadbc1fe62e8ba2d8774470b
Sirisak Lueangsaksri <spywhere@me.com>
2020-12-06 17:26:38 +0700

    fix(tmux): cpu temperature show duplicate unit

--- supports/tmux/scripts/temp.sh
+++ supports/tmux/scripts/temp.sh
@@ -1,5 +1,5 @@
 if command vcgencmd 1>/dev/null 2>&1; then
-  echo $(vcgencmd measure_temp | sed -e "s/temp=//" -e "s/.0'C//")
+  echo $(vcgencmd measure_temp | sed -e "s/temp=//" -e "s/'C//")
 else
   echo ""
 fi
