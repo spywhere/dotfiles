68b63c90e7ed3d55846dcff731b05673d473c7bc
Sirisak Lueangsaksri <spywhere@me.com>
2020-12-07 13:02:56 +0700

    feat(installation): alpine implementation

--- systems/alpine.sh
+++ systems/alpine.sh
@@ -13,11 +13,63 @@ fi
 
 . systems/base.sh
 
+MIRROR="http://dl-cdn.alpinelinux.org/alpine/{branch}/{repo}"
+
+_get_mirror_repo() {
+  local repo="$1"
+  local branch="$2"
+  printf "%s" "$MIRROR" | sed "s/{repo}/$repo/g" | sed "s/{branch}/$branch/g"
+}
+
 update() {
   if test $1 = "upgrade"; then
     cmd apk -U upgrade
   else
     cmd apk update
+  fi
+}
+
+install_packages() {
+  local main_packages=""
+  local community_packages=""
+  local testing_packages=""
+  local edge_packages=""
+  for package in $@; do
+    local repo=$(printf "%s" "$package" | cut -d'|' -f2)
+    local name=$(printf "%s" "$package" | cut -d'|' -f3)
+    case $repo in
+      main)
+        main_packages=$(_add_item "$main_packages" " " "$name")
+        ;;
+      community)
+        community_packages=$(_add_item "$community_packages" " " "$name")
+        ;;
+      testing)
+        testing_packages=$(_add_item "$testing_packages" " " "$name")
+        ;;
+      edge)
+        edge_packages=$(_add_item "$edge_packages" " " "$name")
+        ;;
+      *)
+        warn "unknown repository \"$repo\" for \"$name\""
+        ;;
+    esac
+  done
+  if test -n "$main_packages"; then
+    print "Installing main packages..."
+    cmd apk add $main_packages
+  fi
+  if test -n "$edge_packages"; then
+    print "Installing edge packages..."
+    cmd apk add --repository=$(_get_mirror_repo main edge) $edge_packages
+  fi
+  if test -n "$community_packages"; then
+    print "Installing community packages..."
+    cmd apk add --repository=$(_get_mirror_repo community edge) $community_packages
+  fi
+  if test -n "$testing_packages"; then
+    print "Installing testing packages..."
+    cmd apk add --repository=$(_get_mirror_repo testing edge) $testing_packages
   fi
 }
 
