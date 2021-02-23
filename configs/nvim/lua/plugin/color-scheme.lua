9c1cdada49e7e86dfb8d56aa5a7dfb1eb612642c
Sirisak Lueangsaksri <spywhere@me.com>
2021-02-23 13:48:59 +0700

    feat(nvim): better colorscheme

--- configs/nvim/lua/plugin/color-scheme.lua
+++ configs/nvim/lua/plugin/color-scheme.lua
@@ -6,58 +6,106 @@ local registry = require('lib/registry')
 registry.install('arcticicestudio/nord-vim')
 
 local color_setup = function ()
+  -- color palettes (from nord-vim)
+  -- #2e3440
+  -- #3b4252
+  -- #434c5e
+  -- #4c566a
+  -- #d8dee9
+  -- #e5e9f0
+  -- #eceff4
+
+  -- #8fbcbb
+  -- #88c9d9
+  -- #81a1c1
+  -- #5e81ac
+  -- #bf616a
+  -- #d98770
+  -- #ebcb8b
+  -- #a3be8c
+  -- #b48ead
+  bindings.highlight.define('NordBlack', { guifg='#3b4252' })
+  bindings.highlight.define('NordRed', { guifg='#bf616a' })
+  bindings.highlight.define('NordGreen', { guifg='#a3be8c' })
+  bindings.highlight.define('NordYellow', { guifg='#ebcb8b' })
+  bindings.highlight.define('NordBlue', { guifg='#81a1c1' })
+  bindings.highlight.define('NordMagenta', { guifg='#b48ead' })
+  bindings.highlight.define('NordCyan', { guifg='#88c9d9' })
+  bindings.highlight.define('NordWhite', { guifg='#e5e9f0' })
+
+  bindings.highlight.define('NordBrightBlack', { guifg='#4c566a' })
+  bindings.highlight.define('NordBrightRed', { guifg='#bf616a' })
+  bindings.highlight.define('NordBrightGreen', { guifg='#a3be8c' })
+  bindings.highlight.define('NordBrightYellow', { guifg='#ebcb8b' })
+  bindings.highlight.define('NordBrightBlue', { guifg='#81a1c1' })
+  bindings.highlight.define('NordBrightMagenta', { guifg='#b48ead' })
+  bindings.highlight.define('NordBrightCyan', { guifg='#8fbcbb' })
+  bindings.highlight.define('NordBrightWhite', { guifg='#eceff4' })
+
+  bindings.highlight.define('NordDimBlack', { guifg='#373e4d' })
+  bindings.highlight.define('NordDimRed', { guifg='#94545d' })
+  bindings.highlight.define('NordDimGreen', { guifg='#809575' })
+  bindings.highlight.define('NordDimYellow', { guifg='#b29e75' })
+  bindings.highlight.define('NordDimBlue', { guifg='#68809a' })
+  bindings.highlight.define('NordDimMagenta', { guifg='#8c738c' })
+  bindings.highlight.define('NordDimCyan', { guifg='#6d96a6' })
+  bindings.highlight.define('NordDimWhite', { guifg='#aeb5bb'})
+
+  -- black background
   bindings.highlight.define('Normal', { guibg='#1c1c1c', ctermbg='234' })
+  -- invisible splits
   bindings.highlight.define('SignColumn', { guibg='#1c1c1c', ctermbg='234' });
   bindings.highlight.define('VertSplit', { guifg='bg', ctermfg='bg', guibg='#1c1c1c', ctermbg='234' });
-  bindings.highlight.define('TSError', { ctermfg='203', guifg='#f44747' });
-  bindings.highlight.define('TSPunctDelimiter', { ctermfg='249', guifg='#abb2bf' });
-  bindings.highlight.define('TSPunctBracket', { ctermfg='249', guifg='#abb2bf' });
-  bindings.highlight.define('TSPunctSpecial', { ctermfg='249', guifg='#abb2bf' });
-  bindings.highlight.define('TSConstant', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSConstBuiltin', { ctermfg='173', guifg='#d19a66' });
-  bindings.highlight.define('TSConstMacro', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSStringRegex', { ctermfg='114', guifg='#98c379' });
-  bindings.highlight.define('TSString', { ctermfg='114', guifg='#98c379' });
-  bindings.highlight.define('TSStringEscape', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSCharacter', { ctermfg='114', guifg='#98c379' });
-  bindings.highlight.define('TSNumber', { ctermfg='173', guifg='#d19a66' });
-  bindings.highlight.define('TSBoolean', { ctermfg='173', guifg='#d19a66' });
-  bindings.highlight.define('TSFloat', { ctermfg='114', guifg='#98c379' });
-  bindings.highlight.define('TSAnnotation', { ctermfg='180', guifg='#e5c07b' });
-  bindings.highlight.define('TSAttribute', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSNamespace', { ctermfg='201', guifg='#ff00ff' });
-  bindings.highlight.define('TSFuncBuiltin', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSFunction', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSFuncMacro', { ctermfg='180', guifg='#e5c07b' });
-  bindings.highlight.define('TSParameter', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSParameterReference', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSMethod', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSField', { ctermfg='168', guifg='#e06c75' });
-  bindings.highlight.define('TSProperty', { ctermfg='180', guifg='#e5c07b' });
-  bindings.highlight.define('TSConstructor', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSConditional', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSRepeat', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSLabel', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSKeyword', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSKeywordFunction', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSKeywordOperator', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSOperator', { ctermfg='249', guifg='#abb2bf' });
-  bindings.highlight.define('TSException', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSType', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSTypeBuiltin', { ctermfg='75', guifg='#61afef' });
-  bindings.highlight.define('TSStructure', { ctermfg='201', guifg='#ff00ff' });
-  bindings.highlight.define('TSInclude', { ctermfg='175', guifg='#c586c0' });
-  bindings.highlight.define('TSVariable', { ctermfg='73', guifg='#56b6c2' });
-  bindings.highlight.define('TSVariableBuiltin', { ctermfg='180', guifg='#e5c07b' });
-  bindings.highlight.define('TSText', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSStrong', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSEmphasis', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSUnderline', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSTitle', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSLiteral', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSURI', { ctermfg='226', guifg='#ffff00' });
-  bindings.highlight.define('TSTag', { ctermfg='168', guifg='#e06c75' });
-  bindings.highlight.define('TSTagDelimiter', { ctermfg='241', guifg='#5c6370' });
+
+  -- color definition (taken from https://github.com/arcticicestudio/nord-vim/issues/235)
+  bindings.highlight.link('TSError', 'NordBrightRed')
+  bindings.highlight.link('TSPunctDelimiter', 'NordBlue')
+  bindings.highlight.link('TSPunctBracket', 'NordBlue')
+  bindings.highlight.link('TSPunctSpecial', 'NordBlue')
+  bindings.highlight.link('TSConstant', 'NordCyan')
+  bindings.highlight.link('TSConstBuiltin', 'NordDimYellow')
+  bindings.highlight.link('TSConstMacro', 'NordBrightCyan')
+  bindings.highlight.link('TSStringRegex', 'NordGreen')
+  bindings.highlight.link('TSString', 'NordGreen')
+  bindings.highlight.link('TSStringEscape', 'NordBrightCyan')
+  bindings.highlight.link('TSCharacter', 'NordGreen')
+  bindings.highlight.link('TSNumber', 'NordDimYellow')
+  bindings.highlight.link('TSBoolean', 'NordDimYellow')
+  bindings.highlight.link('TSFloat', 'NordGreen')
+  bindings.highlight.link('TSAnnotation', 'NordYellow')
+  bindings.highlight.link('TSAttribute', 'NordCyan')
+  bindings.highlight.link('TSNamespace', 'NordMagenta')
+  bindings.highlight.link('TSFuncBuiltin', 'NordBlue')
+  bindings.highlight.link('TSFunction', 'NordBlue')
+  bindings.highlight.link('TSFuncMacro', 'NordYellow')
+  bindings.highlight.link('TSParameter', 'NordBrightCyan')
+  bindings.highlight.link('TSParameterReference', 'NordBrightCyan')
+  bindings.highlight.link('TSMethod', 'NordBlue')
+  bindings.highlight.link('TSField', 'NordBrightRed')
+  bindings.highlight.link('TSProperty', 'NordYellow')
+  bindings.highlight.link('TSConstructor', 'NordCyan')
+  bindings.highlight.link('TSConditional', 'NordMagenta')
+  bindings.highlight.link('TSRepeat', 'NordMagenta')
+  bindings.highlight.link('TSLabel', 'NordBlue')
+  bindings.highlight.link('TSKeyword', 'NordMagenta')
+  bindings.highlight.link('TSKeywordFunction', 'NordMagenta')
+  bindings.highlight.link('TSKeywordOperator', 'NordMagenta')
+  bindings.highlight.link('TSOperator', 'NordDimWhite')
+  bindings.highlight.link('TSException', 'NordMagenta')
+  bindings.highlight.link('TSType', 'NordBlue')
+  bindings.highlight.link('TSTypeBuiltin', 'NordBlue')
+  bindings.highlight.link('TSStructure', 'NordBrightMagenta')
+  bindings.highlight.link('TSInclude', 'NordMagenta')
+  bindings.highlight.link('TSVariable', 'NordCyan')
+  bindings.highlight.link('TSVariableBuiltin', 'NordYellow')
+  bindings.highlight.link('TSText', 'NordBrightYellow')
+  bindings.highlight.link('TSEmphasis', 'NordBrightYellow')
+  bindings.highlight.link('TSUnderline', 'NordBrightYellow')
+  bindings.highlight.link('TSTitle', 'NordBrightYellow')
+  bindings.highlight.link('TSLiteral', 'NordBrightYellow')
+  bindings.highlight.link('TSURI', 'NordBrightYellow')
+  bindings.highlight.link('TSTag', 'NordBrightRed')
+  bindings.highlight.link('TSTagDelimiter', 'NordBrightBlack')
 end
 registry.auto('ColorScheme', color_setup)
 
