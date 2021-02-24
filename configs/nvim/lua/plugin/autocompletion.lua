6e9e8b670d295dcb57a570127f90512844d75788
Sirisak Lueangsaksri <spywhere@me.com>
2021-02-24 16:37:56 +0700

    feat(nvim): optional flags for omnisharp

--- configs/nvim/lua/plugin/autocompletion.lua
+++ configs/nvim/lua/plugin/autocompletion.lua
@@ -276,9 +276,18 @@ local setup_lsps = function ()
       pre = install_omnisharp,
       cmd = function (command)
         local pid = fn.getpid()
+        -- solution
+        -- table.insert(command, '-s')
+        -- table.insert(command, '<path to sln file>')
         table.insert(command, '--languageserver')
         table.insert(command, '--hostPID')
         table.insert(command, tostring(pid))
+        -- exclude paths
+        -- table.insert(command, 'FileOptions:SystemExcludeSearchPatterns:<index>=<path>')
+        -- table.insert(command, 'MsBuild:LoadProjectsOnDemand=true')
+        -- table.insert(command, 'RoslynExtensionsOptions:EnableAnalyzersSupport=true')
+        -- table.insert(command, 'FormattingOptions:EnableEditorConfigSupport=true')
+        -- table.insert(command, 'RoslynExtensionsOptions:EnableDecompilationSupport=true')
         return command
       end
     },
