c94673d5ffcb30beca103d5828a7782bb2d730dc
Sirisak Lueangsaksri <spywhere@me.com>
2021-03-03 11:52:47 +0700

    feat(nvim): add quick git status mapping

--- configs/nvim/lua/plugin/git.lua
+++ configs/nvim/lua/plugin/git.lua
@@ -2,5 +2,8 @@ local registry = require('lib/registry')
 
 registry.install('airblade/vim-gitgutter', { lazy = 'vim-gitgutter' })
 registry.install('tpope/vim-fugitive', { lazy = 'vim-fugitive' })
+registry.post(function ()
+  bindings.map.normal('gst', '<cmd>Gstatus<cr>')
+end)
 registry.install('itchyny/vim-gitbranch')
 registry.install('rhysd/git-messenger.vim', { lazy = 'git-messenger.vim' })
