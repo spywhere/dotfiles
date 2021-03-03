abc9be514e7af7bbedef3a8c4b48baf1c2196aee
Sirisak Lueangsaksri <spywhere@me.com>
2021-03-03 11:44:14 +0700

    feat(nvim): configs for gui nvim

--- configs/nvim/lua/editor/gui.lua
+++ configs/nvim/lua/editor/gui.lua
@@ -5,6 +5,18 @@ local windows = function ()
   bindings.map.all('<F11>', '<cmd>call GuiWindowFullScreen(!g:GuiWindowFullScreen)<cr>')
 end
 
-if fn.has('win32') == 1 then
-  registry.pre(windows)
+local gui = function ()
+  -- use TUI tabline instead
+  api.nvim_command('GuiTabline 0')
+  -- use TUI completion menu instead
+  api.nvim_command('GuiPopupmenu 0')
+  api.nvim_command('GuiFont! JetBrainsMono Nerd Font Mono:h9')
+end
+
+if fn.exists('g:GuiLoaded') then
+  registry.defer(gui)
+
+  if fn.has('win32') == 1 then
+    registry.pre(windows)
+  end
 end
