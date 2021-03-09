0c8b40733a60640afe7b3e6c05edeff81043c200
Sirisak Lueangsaksri <spywhere@me.com>
2021-03-09 16:52:34 +0700

    feat(nvim): add session restoration command

---
+++ configs/nvim/lua/editor/session-management.lua
@@ -0,0 +1,19 @@
+local bindings = require('lib/bindings')
+local registry = require('lib/registry')
+local logger = require('lib/logger')
+
+local session_commands = function ()
+  local load_session = {
+    function(modifiers, path)
+      local session_file = path or 'Session.vim'
+      if fn.filereadable(session_file) == 1 then
+        api.nvim_command(table.concat({ 'source', session_file }, ' '))
+      else
+        logger.inline.info(string.format('No session file found (%s)', session_file))
+      end
+    end,
+    '-nargs=*'
+  }
+  bindings.cmd('LoadSession', load_session)
+end
+registry.defer(session_commands)
