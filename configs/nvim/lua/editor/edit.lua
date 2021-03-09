66c4377d5d54e5b4fc98dfe6954fb35fb786be92
Sirisak Lueangsaksri <spywhere@me.com>
2021-03-09 13:52:39 +0700

    refactor(nvim): only use special visual block on wsl

--- configs/nvim/lua/editor/edit.lua
+++ configs/nvim/lua/editor/edit.lua
@@ -41,7 +41,7 @@ local quick_save = function ()
 end
 registry.defer(quick_save)
 
-if fn.has('win32') == 1 or fn.has('wsl') then
+if fn.has('wsl') then
   -- use X for visual block since Ctrl-V is paste
   local alternative_visual_block = function ()
     bindings.map.nv('X', '<C-v>')
