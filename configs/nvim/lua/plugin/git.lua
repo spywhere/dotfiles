98d850b710e3a4fb0fbf917529e4cc65156ad93b
Sirisak Lueangsaksri <spywhere@me.com>
2021-03-11 17:56:22 +0700

    feat(nvim): don't show count for git signs

--- configs/nvim/lua/plugin/git.lua
+++ configs/nvim/lua/plugin/git.lua
@@ -2,6 +2,9 @@ local registry = require('lib/registry')
 local bindings = require('lib/bindings')
 
 registry.install('mhinz/vim-signify', { lazy = 'vim-signify' })
+registry.post(function ()
+  vim.g.signify_sign_show_count = 0
+end)
 registry.install('tpope/vim-fugitive', { lazy = 'vim-fugitive' })
 registry.post(function ()
   bindings.map.normal('gst', '<cmd>Gstatus<cr>')
