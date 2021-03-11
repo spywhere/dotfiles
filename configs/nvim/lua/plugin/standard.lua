e358f7b6bb0903958b0d843a4b1737dd7c9ef4d0
Sirisak Lueangsaksri <spywhere@me.com>
2021-03-11 16:10:19 +0700

    feat(nvim): exclude editorconfig in fugitive and remote file

--- configs/nvim/lua/plugin/standard.lua
+++ configs/nvim/lua/plugin/standard.lua
@@ -1,4 +1,7 @@
 local registry = require('lib/registry')
 
 registry.install('editorconfig/editorconfig-vim')
+registry.post(function ()
+  vim.g.Editorconfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
+end)
 registry.install('tpope/vim-sensible')
