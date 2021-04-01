3ff141ff098668647a0705b4ce8eaf2fff69b5b3
Sirisak Lueangsaksri <spywhere@me.com>
2021-04-01 11:21:26 +0700

    feat(nvim): disable indent guides on terminal buffers

--- configs/nvim/lua/plugin/visualization.lua
+++ configs/nvim/lua/plugin/visualization.lua
@@ -6,6 +6,14 @@ registry.post(function ()
   vim.g.indentLine_leadingSpaceChar = '·'
   vim.g.indentLine_fileTypeExclude = { 'text', 'startify' }
 end)
+registry.defer(function ()
+  local disable_indent_guides = function ()
+    api.nvim_command('IndentLinesDisable')
+  end
+
+  -- disable indentation guides on terminal buffers
+  registry.auto('TermOpen', disable_indent_guides)
+end)
 
 registry.install(
   'jeffkreeftmeijer/vim-numbertoggle',
