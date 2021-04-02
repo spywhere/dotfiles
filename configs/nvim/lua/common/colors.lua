a45a4fe13911a411dc2a4ed5f71467d96e6bdab0
Sirisak Lueangsaksri <spywhere@me.com>
2021-04-02 15:27:06 +0700

    refactor(nvim): allow custom palette lookup

--- configs/nvim/lua/common/colors.lua
+++ configs/nvim/lua/common/colors.lua
@@ -7,7 +7,7 @@ local M = {
 -- #3b4252 238  nord1
 -- #434c5e 239  nord2
 -- #4c566a 240  nord3
--- #d8dee9      nord4 fg
+-- #d8dee9 188  nord4 fg
 -- #e5e9f0 255  nord5
 -- #eceff4 255  nord6
 
@@ -16,16 +16,19 @@ local M = {
 -- #81a1c1 109  nord9
 -- #5e81ac  67  nord10
 -- #bf616a 131  nord11
--- #d98770      nord12
+-- #d08770 173  nord12
 -- #ebcb8b 222  nord13
 -- #a3be8c 144  nord14
 -- #b48ead 139  nord15
 --
 -- #616e88  60  nord3-bright
 -- #5c6370 241  line-gray
--- #2c323c      cursor-gray
+-- #2c323c  23  cursor-gray
 -- #3b4048 238  special-gray
 M.nord = {
+  orange = { gui='#d08770', cterm='173' },
+  lightblue = { gui='#5e81ac', cterm='67' },
+
   black = { gui='#3b4252', cterm='238' },
   red = { gui='#bf616a', cterm='131' },
   green = { gui='#a3be8c', cterm='144' },
@@ -54,11 +57,12 @@ M.nord = {
   dimwhite = { gui='#aeb5bb' }
 }
 
-M.resolve_color = function (name)
-  if not M.nord[name] then
+M.resolve_color = function (name, palette)
+  local definitions = palette or M.nord
+  if not definitions[name] then
     return { gui='NONE', cterm='NONE' }
   end
-  local color = M.nord[name]
+  local color = definitions[name]
   if type(color) == 'string' then
     return M.resolve_color(color)
   end
@@ -70,12 +74,12 @@ M.group = function (fg, bg, palette)
   local group = {}
 
   if type(fg) == 'string' and definitions[fg] then
-    local color = M.resolve_color(fg)
+    local color = M.resolve_color(fg, palette)
     group.guifg=color.gui
     group.ctermfg=color.cterm
   end
   if type(bg) == 'string' and definitions[bg] then
-    local color = M.resolve_color(bg)
+    local color = M.resolve_color(bg, palette)
     group.guibg=color.gui
     group.ctermbg=color.cterm
   end
