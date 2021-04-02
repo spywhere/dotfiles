c97a7d5d715a649fe4cfd6b9df6233d0b1b11b17
Sirisak Lueangsaksri <spywhere@me.com>
2021-04-02 15:27:49 +0700

    feat(nvim): use galaxy.nvim instead of lightline.vim

--- configs/nvim/lua/plugin/appearance.lua
+++ configs/nvim/lua/plugin/appearance.lua
@@ -1,25 +1,23 @@
 local registry = require('lib/registry')
 local bindings = require('lib/bindings')
+local colors = require('common/colors')
 
-registry.install('itchyny/lightline.vim')
+registry.install('glepnir/galaxyline.nvim')
 registry.post(function ()
   local components = {
-    Mode = function ()
-      return fn['lightline#mode']()
-    end,
     Branch = function ()
       return fn['gitbranch#name']()
     end,
     Readonly = function ()
       if vim.bo.readonly then
-        return '+'
+        return '[RO]'
       else
         return ''
       end
     end,
     Modified = function ()
       if vim.bo.modified then
-        return '+'
+        return '[+]'
       else
         return ''
       end
@@ -42,25 +40,35 @@ registry.post(function ()
           end
         end
 
-        if winwidth < 60 + name:len() then
+        if winwidth < 70 + name:len() then
           table.remove(list, 1)
           return fallback_name(winwidth, list)
         end
 
-        return name
+        if name == '' then
+          return '[no name]'
+        else
+          return name
+        end
       end
 
-      return fallback_name(fn.winwidth(0), {
+      local value = fallback_name(fn.winwidth(0), {
         function () return fn.expand('%:f') end,
         function () return fn.pathshorten(fn.expand('%:f')) end,
         function () return fn.expand('%:t') end
       })
+
+      if value == '' then
+        return ''
+      end
+
+      return value .. ' '
     end,
     LineInfo = function ()
-      return fn.line('.') .. ':' .. fn.col('.')
+      return fn.printf('%3d:%-2d', fn.col('.'), fn.line('.'))
     end,
     Percent = function ()
-      return math.floor(fn.line('.') * 100 / fn.line('$')) .. '%'
+      return fn.printf('%3d%%', math.floor(fn.line('.') * 100 / fn.line('$')))
     end,
     FileFormat = function ()
       return vim.bo.fileformat
@@ -70,6 +78,9 @@ registry.post(function ()
     end,
     FileType = function ()
       return vim.bo.filetype
+    end,
+    Clock = function ()
+      return os.date('%d %b %y %H:%M')
     end
   }
 
@@ -98,102 +109,329 @@ registry.post(function ()
     end
   end
 
-  for name, func in pairs(components) do
-    registry.fn(
-      { name = 'Lightline' .. name },
-      component(name)
-    )
+  local statusline = {
+    bg='brightblack',
+    fg='white'
+  }
+
+  local refine_section = function (palette, sections, inactive)
+    local line = {}
+
+    local section_map = function (key, section)
+      if section.inactive == false and inactive then
+        return
+      end
+
+      local provider
+      local raw_provider
+      local nohighlight = true
+
+      if type(section) == 'string' then
+        provider = section
+        section = {}
+      elseif type(section) == 'table' then
+        if components[section.component] ~= nil then
+          raw_provider = component(section.component)
+        else
+          provider = section.provider
+        end
+        nohighlight = section.nohighlight or false
+      end
+
+      local transform_provider = function (provider, separator)
+        if separator == nil then
+          return provider
+        end
+
+        return function ()
+          local value = provider() or ''
+          if value == '' then
+            return ''
+          else
+            return (separator.left or '') .. value .. (separator.right or '')
+          end
+        end
+      end
+
+      if type(provider) == 'string' then
+        raw_provider = transform_provider(function () return provider end, section.separator)
+      elseif raw_provider == nil then
+        raw_provider = transform_provider(provider, section.separator)
+      else
+        raw_provider = transform_provider(raw_provider, section.separator)
+      end
+
+      local component = {
+        provider = raw_provider,
+        icon = section.icon,
+        condition = section.condition
+      }
+
+      if not nohighlight then
+        component.highlight = {
+          colors.resolve_color(section.foreground or palette.fg).gui,
+          colors.resolve_color(section.background or palette.bg).gui
+        }
+      end
+
+      table.insert(line, {
+        [key] = component
+      })
+    end
+
+    -- loop through list
+    for key, value in ipairs(sections) do
+      if type(key) == 'number' then
+        -- each item is an object with one key
+        for name, section in pairs(value) do
+          section_map(name, section)
+          break
+        end
+      end
+    end
+    return line
   end
 
-  local lightline = {
-    colorscheme = 'nord',
-    tabline = {
-      left = {
-        { 'buffers' }
+  local sections = {
+    left = {
+      {
+        ModeLeft = {
+          provider = ''
+        }
       },
-      right = {
-        {}
+      {
+        Mode = {
+          provider = function ()
+            local mode_map = {
+              n = { alias='NOM', color='cyan' },
+              i = { alias='INS', color='white' },
+              v = { alias='VIS', color='green' },
+              V = { alias='V-L', color='green' },
+              ['CTRL-V'] = { alias='V-B', color='green' },
+              c = { alias='CMD', color='cyan' },
+              t = { alias='TRM', color='white' },
+              s = { alias='SEL', color='brightcyan' },
+              S = { alias='S-L', color='brightcyan' },
+              ['CTRL-S'] = { alias='S-B', color='brightcyan' },
+              R = { alias='REP', color='yellow' }
+            }
+
+            local mode = mode_map[fn.mode()] or {
+              alias=fn.mode(),
+              color='red'
+            }
+            bindings.highlight.define('GalaxyModeLeft', colors.group(mode.color, 'black'))
+            bindings.highlight.define('GalaxyModeRight', colors.group(mode.color, 'black'))
+            bindings.highlight.define('GalaxyMode', colors.group('black', mode.color))
+            bindings.highlight.define('GalaxyGitBranch', colors.group('black', mode.color))
+
+            return mode.alias
+          end
+        },
+      },
+      {
+        GitBranch = {
+          component = 'Branch',
+          separator = {
+            left = '  | '
+          },
+          inactive = false
+        }
+      },
+      {
+        ModeRight = {
+          provider = ''
+        }
+      },
+      {
+        Spacing = {
+          provider = ' ',
+          background = 'black'
+        }
+      },
+      {
+        Readonly = {
+          component = 'Readonly',
+          separator = {
+            left = ' | '
+          }
+        }
+      },
+      {
+        RelativePath = {
+          component = 'RelativePath',
+          background = 'black'
+        }
+      },
+      {
+        Modified = {
+          component = 'Modified'
+        }
       }
     },
-    component_raw = {
-      buffers = 1
-    },
-    component_expand = {
-      linter_hints = 'lightline#lsp#hints',
-      linter_infos = 'lightline#lsp#infos',
-      linter_warnings = 'lightline#lsp#warnings',
-      linter_errors = 'lightline#lsp#errors',
-      linter_ok = 'lightline#lsp#ok',
-      buffers = 'lightline#bufferline#buffers'
-    },
-    component_type = {
-      linter_hints = 'right',
-      linter_infos = 'right',
-      linter_warnings = 'warning',
-      linter_errors = 'error',
-      linter_ok = 'tabsel',
-      buffers = 'tabsel'
-    },
-    component_function = {
-      obsession = 'ObsessionStatus',
-      gitbranch = 'LightlineBranch',
-      mode = 'LightlineMode',
-      readonly = 'LightlineReadonly',
-      modified = 'LightlineModified',
-      relativepath = 'LightlineRelativePath',
-      lineinfo = 'LightlineLineInfo',
-      percent = 'LightlinePercent',
-      fileformat = 'LightlineFileFormat',
-      fileencoding = 'LightlineFileEncoding',
-      filetype = 'LightlineFileType'
-    },
-    inactive = {
-      left = {
-        {},
-        { 'relativepath' }
+    right = {
+      {
+        ObsessionStatus = {
+          provider = fn.ObsessionStatus,
+          separator = {
+            right = ' '
+          },
+          inactive = false
+        }
       },
-      right = {
-        { 'lineinfo' },
-        { 'percent' },
-        {
-          'filetype'
-        },
-      }
-    },
-    active = {
-      left = {
-        { 'mode', 'paste' },
-        { 'gitbranch', 'readonly', 'relativepath', 'modified' }
+      {
+        FileType = {
+          component = 'FileType'
+        }
       },
-      right = {
-        {
-          'linter_ok',
-          'linter_errors',
-          'linter_warnings',
-          'linter_infos',
-          'linter_hints'
-        },
-        { 'lineinfo' },
-        { 'percent' },
-        {
-          'fileformat',
-          'fileencoding',
-          'filetype'
-        },
-        { 'obsession' }
+      {
+        FileEncoding = {
+          component = 'FileEncoding',
+          separator = {
+            left = ' | '
+          }
+        }
+      },
+      {
+        FileFormat = {
+          component = 'FileFormat',
+          separator = {
+            left = ' | ',
+            right = ' '
+          }
+        }
+      },
+      {
+        LinePercent = {
+          component = 'Percent',
+          background = 'black',
+          separator = {
+            left = '  ',
+            right = ' '
+          }
+        }
+      },
+      {
+        LineInfo = {
+          component = 'LineInfo',
+          background = 'black',
+          separator = {
+            left = ' ',
+            right = ' '
+          }
+        }
+      },
+      {
+        Spacing = {
+          provider = ' ',
+          background = 'black'
+        }
+      },
+      {
+        DiagnosticHint = {
+          provider = require('galaxyline.provider_diagnostic').get_diagnostic_hint,
+          icon = '  H:',
+          foreground = 'black',
+          background = 'cyan',
+          inactive = false
+        }
+      },
+      {
+        DiagnosticInfo = {
+          provider = require('galaxyline.provider_diagnostic').get_diagnostic_info,
+          icon = '  I:',
+          foreground = 'black',
+          background= 'green',
+          inactive = false
+        }
+      },
+      {
+        DiagnosticWarn = {
+          provider = require('galaxyline.provider_diagnostic').get_diagnostic_warn,
+          icon = '  W:',
+          foreground = 'black',
+          background = 'orange',
+          inactive = false
+        }
+      },
+      {
+        DiagnosticError = {
+          provider = require('galaxyline.provider_diagnostic').get_diagnostic_error,
+          icon = '  E:',
+          background = 'red',
+          inactive = false
+        }
+      },
+      {
+        DiagnosticOK = {
+          provider = function ()
+            if vim.tbl_isempty(vim.lsp.buf_get_clients(0)) then
+              return ''
+            end
+
+            local active_clients = vim.lsp.get_active_clients()
+            if vim.tbl_isempty(active_clients) then
+              return ''
+            end
+
+            local types = { 'Error', 'Warning', 'Info', 'Hint' }
+            for _, diag_type in ipairs(types) do
+              for _, client in ipairs(active_clients) do
+                if vim.lsp.diagnostic.get_count(api.nvim_get_current_buf(),diag_type,client.id) > 0 then
+                  return ''
+                end
+              end
+            end
+
+            return 'OK '
+          end,
+          icon = '  ',
+          foreground = 'black',
+          background = 'cyan',
+          inactive = false
+        }
       }
     }
   }
 
-  vim.g.lightline = lightline
+  local is_gui = fn.exists('g:GuiLoaded') ~= 1
+  if is_gui then
+    table.insert(sections.right, {
+      ClockLeft = {
+        provider = '',
+        foreground = 'lightblue',
+        background = 'black',
+        inactive = false
+      }
+    })
+    table.insert(sections.right, {
+      Clock = {
+        component = 'Clock',
+        background = 'lightblue',
+        inactive = false
+      }
+    })
+    table.insert(sections.right, {
+      ClockRight = {
+        provider = '',
+        foreground = 'lightblue',
+        background = 'black',
+        inactive = false
+      }
+    })
+  end
+
+  local gl = require('galaxyline')
+  gl.short_line_list = { '' }
+  gl.section = {
+    mid = {},
+    short_line_left = refine_section(statusline, sections.left, true),
+    short_line_right = refine_section(statusline, sections.right, true),
+    left = refine_section(statusline, sections.left),
+    right = refine_section(statusline, sections.right)
+  }
 end)
 
--- registry.install('mengelbrecht/lightline-bufferline')
--- registry.post(function ()
-  -- vim.g['lightline#bufferline#enable_devicons'] = 1
-  -- vim.g['lightline#bufferline#min_buffer_count'] = 2
-  -- vim.g['lightline#bufferline#clickable'] = 1
--- end)
 registry.install('akinsho/nvim-bufferline.lua')
 registry.defer(function ()
   local is_gui = fn.exists('g:GuiLoaded') == 1
@@ -219,7 +457,6 @@ registry.defer(function ()
   vim.defer_fn(hide_tabline, 0)
 end)
 
-registry.install('spywhere/lightline-lsp')
 registry.install('mhinz/vim-startify')
 registry.post(function ()
   vim.g.startify_files_number = 20
