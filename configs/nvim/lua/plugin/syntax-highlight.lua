155e363206cd611a5399ff13efcbd7122d4c99fe
Sirisak Lueangsaksri <spywhere@me.com>
2021-04-15 15:50:05 +0700

    refactor(nvim): ignore unused parsers

--- configs/nvim/lua/plugin/syntax-highlight.lua
+++ configs/nvim/lua/plugin/syntax-highlight.lua
@@ -25,6 +25,10 @@ if fn.has('nvim-0.5') == 1 then
   registry.post(function ()
     require('nvim-treesitter.configs').setup {
       ensure_installed = 'all',
+      ignore_install = {
+        'julia', 'kotlin', 'ocamel', 'ocaml_interface',
+        'ocamllex', 'verilog', 'zig'
+      },
       highlight = {
         enable = true
       }
