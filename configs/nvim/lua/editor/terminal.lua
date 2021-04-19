13ae71e8a123afb60435770e88e02facccb2fe36
Sirisak Lueangsaksri <spywhere@me.com>
2021-04-19 14:28:47 +0700

    feat(nvim): add horizontal split for terminal

--- configs/nvim/lua/editor/terminal.lua
+++ configs/nvim/lua/editor/terminal.lua
@@ -17,6 +17,7 @@ if fn.has('win32') == 1 and fn.executable('wsl.exe') == 1 then
     }
     bindings.cmd('WSLTerminal', wsl_terminal)
     bindings.map.normal('<C-a>c', '<cmd>WSLTerminal<cr>')
+    bindings.map.normal('<C-a>-', '<cmd>split | WSLTerminal<cr>')
     bindings.map.normal('<C-a><bar>', '<cmd>vsplit | WSLTerminal<cr>')
     bindings.map.normal('<C-a><space>', '<cmd>WSLTerminal<cr>')
   end
