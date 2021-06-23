5d95745a3bb4714ad3a089c5a4328bea1d563262
Sirisak Lueangsaksri <spywhere@me.com>
2021-06-24 01:31:37 +0700

    feat(font): improve font installation experience

--- setup/font.sh
+++ setup/font.sh
@@ -11,15 +11,17 @@ then
   exit 1
 fi
 
+require curl
+
 add_setup 'setup_font'
 
 fonts=""
 add_font() {
-  local name="$(printf "%s" "$1" | sed 's/ /%20/g')"
-  local file="$(printf "%s" "$2" | sed 's/ /%20/g')"
-  local url="$3"
-
-  fonts=$(_add_item "$fonts" ";" "$name|$file|$url")
+  if test -n "$1"; then
+    field name "$1"
+  fi
+  fonts="$(_add_to_list "$fonts" "$(make_object)")"
+  reset_object
 }
 
 install_fonts() {
@@ -31,11 +33,12 @@ install_fonts() {
 
   local missing_fonts=""
 
-  for font_info in $(_split "$fonts"); do
-    local file=$(printf "%s" "$font_info" | cut -d'|' -f2 | sed 's/%20/ /g')
+  eval "set -- $fonts"
+  for font_info in "$@"; do
+    local file="$(parse_field "$font_info" file)"
 
     if ! test -f "$font_path/$file"; then
-      missing_fonts=$(_add_item "$missing_fonts" ";" "$font_info")
+      missing_fonts="$(_add_item "$missing_fonts" ";" "$font_info")"
     fi
   done
 
@@ -44,20 +47,25 @@ install_fonts() {
     if ! test -d "$font_path"; then
       mkdir -p "$font_path"
     fi
-    for font_info in $(_split "$missing_fonts"); do
-      local name=$(printf "%s" "$font_info" | cut -d'|' -f1 | sed 's/%20/ /g')
-      local file=$(printf "%s" "$font_info" | cut -d'|' -f2 | sed 's/%20/ /g')
-      local url=$(printf "%s" "$font_info" | cut -d'|' -f3)
+    for font_info in "$@"; do
+      local name="$(parse_field "$font_info" name)"
+      local file="$(parse_field "$font_info" file)"
+      local url="$(parse_field "$font_info" url)"
+      if test -z "$name"; then
+        name="$file"
+      fi
 
-      step "Installing $name..."
-      cmd curl -fLo "$font_path/$file" "$url"
+      step "Downloading and installing $name..."
+      cmd curl -sfLo "$font_path/$file" "$url"
     done
   fi
 }
 
 # install fonts
 setup_font() {
-  add_font "JetBrains Mono Nerd Font" "JetBrains Mono Regular Nerd Font Complete.ttf" "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf?raw=true"
+  field file "JetBrains Mono Regular Nerd Font Complete.ttf"
+  field url "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf?raw=true"
+  add_font "JetBrains Mono Nerd Font"
 
   install_fonts
 }
