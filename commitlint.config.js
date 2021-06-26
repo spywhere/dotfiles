ddc5d19642ee0b0d7a60dca27c40fd1531fe49b2
Sirisak Lueangsaksri <spywhere@me.com>
2021-06-26 17:06:25 +0700

    feat(git): commit message require a subject

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,7 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
+    'scope-empty': [2, 'never'],
     'subject-case': [
       2,
       'never',
