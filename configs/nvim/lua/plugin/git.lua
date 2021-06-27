244fa912d02f455d3760dcbcd00d82007d2ded0b
Sirisak Lueangsaksri <spywhere@me.com>
2021-06-28 03:49:14 +0700

    feat(nvim): add neogit as experimental plugin

--- configs/nvim/lua/plugin/git.lua
+++ configs/nvim/lua/plugin/git.lua
@@ -8,15 +8,21 @@ registry.install {
     vim.g.signify_sign_show_count = 0
   end
 }
+
 registry.install {
   'tpope/vim-fugitive',
+  -- experimental
+  -- 'TimUntersberger/neogit',
   lazy = true,
   config = function ()
     bindings.map.normal('gst', '<cmd>Git<cr>')
+    -- bindings.map.normal('gst', '<cmd>Neogit<cr>')
     bindings.map.normal('gdd', '<cmd>Git diff<cr>')
+    -- bindings.map.normal('gdd', '<cmd>Neogit diff<cr>')
     bindings.map.normal('gds', '<cmd>Git diff --staged<cr>')
   end
 }
+
 registry.install('itchyny/vim-gitbranch')
 registry.install {
   'rhysd/git-messenger.vim',
