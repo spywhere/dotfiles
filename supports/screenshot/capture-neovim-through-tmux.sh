e0a36be13e57c9b5abdcac04f02e9389fc07fb1e
Sirisak Lueangsaksri <spywhere@me.com>
2021-07-06 01:36:17 +0700

    feat(dots): add script for generating screenshot image

---
+++ supports/screenshot/capture-neovim-through-tmux.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+OLD_TMUX="$TMUX"
+unset TMUX
+
+DESTROY_UNATTACHED="$(tmux show-option -g destroy-unattached)"
+
+tmux set -g destroy-unattached off
+
+# Start neovim
+tmux new-session -d -s ss 'nvim'
+
+# Make sure it's done loading
+sleep 1
+
+# Capture as a text file
+# tmux capture-pane -p -e -C -J -t ss:1 > nvim-ss.txt
+
+# Capture as a HTML file
+tmux2html ss:0 -o nvim-ss.html
+# Convert into image
+wkhtmltoimage nvim-ss.html nvim-ss.png
+rm -f nvim-ss.html
+
+tmux kill-session -t ss
+
+tmux set -g $DESTROY_UNATTACHED
+
+export TMUX="$OLD_TMUX"
