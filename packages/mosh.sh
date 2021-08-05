09b49435b951c583901ee8642eb5b47eb6cb2751
Sirisak Lueangsaksri <spywhere@me.com>
2021-08-06 06:18:44 +0700

    docs(mosh): add note for 24-bit issue

--- packages/mosh.sh
+++ packages/mosh.sh
@@ -11,6 +11,8 @@ then
   exit 1
 fi
 
+# Mosh will need to be manually build to fix 24-bit colors issue
+# Ref: https://github.com/mobile-shell/mosh/issues/961#issuecomment-565741393%22
 use_apt 'mosh'
 use_brew formula 'mosh'
 use_docker_build
