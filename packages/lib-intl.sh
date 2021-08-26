773e19a726d4874a454d20a27964f752838db337
Sirisak Lueangsaksri <spywhere@me.com>
2021-08-26 17:25:40 +0700

    feat(packages): add neovim rebuild dependencies

---
+++ packages/lib-intl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+if
+  ! (command -v force_print >/dev/null 2>&1) ||
+  ! (force_print 3 a b >/dev/null 2>&1) ||
+  test "$(force_print 3 a b)" != "a  b";
+then
+  printf "Please run this script through \"install.sh\" instead"
+  exit 1
+fi
+
+# neovim rebuild dependencies
+use_apk 'main' 'musl-libintl'
---
+++ packages/ninja.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+if
+  ! (command -v force_print >/dev/null 2>&1) ||
+  ! (force_print 3 a b >/dev/null 2>&1) ||
+  test "$(force_print 3 a b)" != "a  b";
+then
+  printf "Please run this script through \"install.sh\" instead"
+  exit 1
+fi
+
+# neovim rebuild dependencies
+use_apk 'main' 'ninja'
+use_apt 'ninja-build'
