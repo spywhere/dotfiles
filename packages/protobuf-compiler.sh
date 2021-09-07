001cdd1e5204dd23ecb11fa0bc38a3d82efd6e1d
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2021-09-08 02:15:33 +0700

    feat(packages): add debian packages for rebuild mosh

---
+++ packages/protobuf-compiler.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+if
+  ! (command -v force_print >/dev/null 2>&1) ||
+  ! (force_print 3 a b >/dev/null 2>&1) ||
+  test "$(force_print 3 a b)" != "a  b";
+then
+  printf "Please run this script through \"install.sh\" instead"
+  exit 1
+fi
+
+# mosh rebuild dependencies
+use_apt 'protobuf-compiler'
