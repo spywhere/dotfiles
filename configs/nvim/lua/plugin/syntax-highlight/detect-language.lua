2f55e07cde30da598a9feca5c801417ecb245f91
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2021-09-16 00:55:35 +0700

    feat(nvim): add detect-language plugin

---
+++ configs/nvim/lua/plugin/syntax-highlight/detect-language.lua
@@ -0,0 +1,10 @@
+local registry = require('lib/registry')
+
+if fn.has('nvim-0.5') == 1 then
+  registry.install {
+    'spywhere/detect-language.nvim',
+    config = function ()
+      require('detect-language').setup {}
+    end
+  }
+end
