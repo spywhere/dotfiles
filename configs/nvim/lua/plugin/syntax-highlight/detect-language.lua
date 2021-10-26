4d68179f214bb1ceed6eaf67ddad09848d5f9c8d
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2021-10-26 23:42:36 +0700

    refactor(nvim): disable auto detection until async is supported

--- configs/nvim/lua/plugin/syntax-highlight/detect-language.lua
+++ configs/nvim/lua/plugin/syntax-highlight/detect-language.lua
@@ -4,7 +4,9 @@ if fn.has('nvim-0.5') == 1 then
   registry.install {
     'spywhere/detect-language.nvim',
     config = function ()
-      require('detect-language').setup {}
+      require('detect-language').setup {
+        events = {}
+      }
     end
   }
 end
