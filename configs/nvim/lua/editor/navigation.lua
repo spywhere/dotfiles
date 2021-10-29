1232c997e18dc08222e50ccb74f63e7a3741bd10
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2021-10-29 12:33:08 +0700

    feat(nvim): add quick close to more filetype

--- configs/nvim/lua/editor/navigation.lua
+++ configs/nvim/lua/editor/navigation.lua
@@ -14,10 +14,17 @@ local horizontal_scrolling = function ()
 end
 registry.defer(horizontal_scrolling)
 
-local quickfix_keymap = function ()
-  local map_quickfix = function ()
+local quick_close = function ()
+  local map_quick_close = function ()
     -- quick close
     bindings.map.buffer.normal('q', '<cmd>wincmd q<cr>')
+  end
+  registry.auto('FileType', map_quick_close, { 'qf', 'help', 'lspinfo' })
+end
+registry.defer_first(quick_close)
+
+local quickfix_keymap = function ()
+  local map_quickfix = function ()
     -- easy split navigations (not working?)
     bindings.map.buffer.normal('<C-h>', '<cmd>wincmd h<cr>')
     bindings.map.buffer.normal('<C-j>', '<cmd>wincmd j<cr>')
