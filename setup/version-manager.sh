5d7bdde4d4daa9695428fdff3b6ee27aee8b32fa
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2021-12-10 23:28:12 +0700

    fix(asdf): workaround for missing plugins

--- setup/version-manager.sh
+++ setup/version-manager.sh
@@ -27,8 +27,8 @@ add_setup 'setup_version_manager'
 
 setup_version_manager() {
   setup_version_manager__plugins="deno fzf golang neovim nodejs python rust shellcheck"
-  set +e
   step "Setting up version manager plugins..."
+  set +e
   for setup_version_manager__plugin in $setup_version_manager__plugins; do
     bash -c ". $HOME/.asdf/asdf.sh && asdf plugin add $setup_version_manager__plugin"
   done
@@ -36,6 +36,15 @@ setup_version_manager() {
   bash "$HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring"
   cmd cd "$HOME"
   step "Installing version manager plugins..."
-  bash -c '. $HOME/.asdf/asdf.sh && asdf install'
+  # asdf v0.9 will now error if the plugin is missing from .tool-versions
+  #   Ref: https://github.com/asdf-vm/asdf/issues/574
+  # Possible solutions:
+  #   - A new install flag: https://github.com/asdf-vm/asdf/issues/968#issuecomment-991106501
+  setup_version_manager__plugins="nodejs python fzf neovim"
+  set +e
+  for setup_version_manager__plugin in $setup_version_manager__plugins; do
+    bash -c ". $HOME/.asdf/asdf.sh && asdf install $setup_version_manager__plugin"
+  done
+  set -e
   cmd cd "$CURRENT_DIR"
 }
