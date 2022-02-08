74c976599e79644047770cdf08988ceb7b031a63
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-02-09 04:31:41 +0700

    feat(packages): add podman

---
+++ packages/podman.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if
+  ! (command -v force_print >/dev/null 2>&1) ||
+  ! (force_print 3 a b >/dev/null 2>&1) ||
+  test "$(force_print 3 a b)" != "a  b";
+then
+  printf "Please run this script through \"install.sh\" instead"
+  exit 1
+fi
+
+has_executable 'podman'
+
+use_apk 'community' 'podman'
+use_apt 'podman'
+use_pacman 'podman'
+use_brew formula 'podman'
