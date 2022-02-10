f7fad54f714338c79a9b63f56ce3286581d40194
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-02-10 22:21:39 +0700

    feat(nvim): configure deno language server

--- configs/nvim/lua/lsp/deno.lua
+++ configs/nvim/lua/lsp/deno.lua
@@ -2,3 +2,13 @@ local lsp = require('lib.lsp')
 
 lsp.setup('denols')
   .need_executable('deno')
+  .options({
+    settings = {
+      deno = {
+        config = 'deno.json',
+        importMap = 'import.json',
+        enable = true,
+        lint = true
+      }
+    }
+  })
