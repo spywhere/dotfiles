5b2f9d0f940b14f4d6692b7411e212f883e00645
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-03-18 23:01:57 +0700

    feat(nvim): only sync install treesitter in linux

--- configs/nvim/lua/plugin/syntax-highlight/treesitter.lua
+++ configs/nvim/lua/plugin/syntax-highlight/treesitter.lua
@@ -7,7 +7,7 @@ registry.install {
   },
   config = function ()
     require('nvim-treesitter.configs').setup {
-      sync_install = true,
+      sync_install = fn.has('linux') == 1,
       ensure_installed = 'all',
       ignore_install = {
         'julia', 'haskell', 'kotlin', 'ocamel', 'ocaml_interface',
