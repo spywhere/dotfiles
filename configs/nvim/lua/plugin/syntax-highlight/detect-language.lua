6b05cd9a7c404a2acc0b85a15bc55d1d54ff319d
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-03-18 22:59:59 +0700

    refactor(nvim): remove neovim version check

--- configs/nvim/lua/plugin/syntax-highlight/detect-language.lua
+++ configs/nvim/lua/plugin/syntax-highlight/detect-language.lua
@@ -1,12 +1,10 @@
 local registry = require('lib.registry')
 
-if fn.has('nvim-0.5') == 1 then
-  registry.install {
-    'spywhere/detect-language.nvim',
-    config = function ()
-      require('detect-language').setup {
-        events = {}
-      }
-    end
-  }
-end
+registry.install {
+  'spywhere/detect-language.nvim',
+  config = function ()
+    require('detect-language').setup {
+      events = {}
+    }
+  end
+}
--- configs/nvim/lua/plugin/syntax-highlight/treesitter.lua
+++ configs/nvim/lua/plugin/syntax-highlight/treesitter.lua
@@ -1,23 +1,21 @@
 local registry = require('lib.registry')
 
-if fn.has('nvim-0.5') == 1 then
-  registry.install {
-    'nvim-treesitter/nvim-treesitter',
-    options = {
-      ['do'] = ':TSUpdateSync'
-    },
-    config = function ()
-      require('nvim-treesitter.configs').setup {
-        sync_install = true,
-        ensure_installed = 'all',
-        ignore_install = {
-          'julia', 'haskell', 'kotlin', 'ocamel', 'ocaml_interface',
-          'ocamllex', 'verilog', 'zig'
-        },
-        highlight = {
-          enable = true
-        }
+registry.install {
+  'nvim-treesitter/nvim-treesitter',
+  options = {
+    ['do'] = ':TSUpdateSync'
+  },
+  config = function ()
+    require('nvim-treesitter.configs').setup {
+      sync_install = true,
+      ensure_installed = 'all',
+      ignore_install = {
+        'julia', 'haskell', 'kotlin', 'ocamel', 'ocaml_interface',
+        'ocamllex', 'verilog', 'zig'
+      },
+      highlight = {
+        enable = true
       }
-    end
-  }
-end
+    }
+  end
+}
