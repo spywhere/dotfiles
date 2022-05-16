22faf8d851db4f50d8434b5568d2d4283239d1af
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-05-16 13:19:00 +0700

    feat(nvim): support setting filter folder via explorer

--- configs/nvim/lua/plugin/explorer/tree.lua
+++ configs/nvim/lua/plugin/explorer/tree.lua
@@ -1,5 +1,6 @@
 local bindings = require('lib.bindings')
 local registry = require('lib.registry')
+local cache = require('lib.cache')
 
 registry.install {
   'kyazdani42/nvim-tree.lua',
@@ -24,7 +25,31 @@ registry.install {
     bindings.map.all('<leader>E', '<cmd>NvimTreeFindFile<cr>')
   end,
   delay = function ()
+    local update_filter_folder = function (node)
+      if node.name == '..' then
+        print('Filter cleared')
+        cache.del('filter_folder')
+        return
+      elseif not node.fs_stat or node.fs_stat.type ~= 'directory' then
+        return
+      end
+
+      cache.set('filter_folder', node.absolute_path)
+      print('Filter:', node.absolute_path)
+    end
+
     require('nvim-tree').setup {
+      view = {
+        mappings = {
+          list = {
+            {
+              key = 'f',
+              action = 'update_filter_folder',
+              action_cb = update_filter_folder
+            }
+          }
+        }
+      },
       update_focused_file = {
         enable = true
       },
