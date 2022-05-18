02ca7214d031485020c92e1ae41856a3ac960d3d
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-05-18 18:22:34 +0700

    feat(nvim): improve provider loading

--- configs/nvim/lua/editor/misc.lua
+++ configs/nvim/lua/editor/misc.lua
@@ -2,5 +2,8 @@ local registry = require('lib.registry')
 
 local leader_key = function ()
   vim.g.mapleader = ','
+
+  vim.g.loaded_ruby_provider = 0
+  vim.g.loaded_perl_provider = 0
 end
 registry.pre(leader_key)
