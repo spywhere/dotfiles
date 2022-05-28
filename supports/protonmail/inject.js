150d4e7c0e27984dd7caf0528801ce9e70ac1f15
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-05-29 02:58:46 +0700

    feat(packages): add mailto support to protonmail

--- supports/protonmail/inject.js
+++ supports/protonmail/inject.js
@@ -1,4 +1,10 @@
 // Ref: https://github.com/electron/electron/issues/31018
 // Electron really screwed up here. atob and btoa are broken in recent versions, so override them.
- window.atob = data => Buffer.from(data, "base64").toString("latin1");
- window.btoa = data => Buffer.from(data, "latin1").toString("base64");
+window.atob = data => Buffer.from(data, "base64").toString("latin1");
+window.btoa = data => Buffer.from(data, "latin1").toString("base64");
+
+const { ipcRenderer } = require('electron');
+
+ipcRenderer.on('open-url', (event, url) => {
+  window.location.hash = 'mailto=' + url;
+});
