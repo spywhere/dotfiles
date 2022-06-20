a7639328f4f7f8647e4a01b7794349a67a62bc3d
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-06-21 01:05:06 +0700

    refactor(nvim): add missing plugins in experiments

--- configs/nvim/lua/plugin/experiments.lua
+++ configs/nvim/lua/plugin/experiments.lua
@@ -1,6 +1,6 @@
 local registry = require('lib.registry')
 
--- Experiment between scrollbar.nvim and nvim-scrollview
+-- Experiment between scrollbar.nvim, nvim-scrollview and satellite.nvim
 registry.experiment('scroll', 'satellite')
 -- Experiment between fzf-lua and telescope
 registry.experiment('fzf', fn.has('win32') == 0)
