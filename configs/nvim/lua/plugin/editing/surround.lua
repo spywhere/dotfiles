5823f2d91e89de2ad01be25fefeaf0c3d24a3202
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-07-02 22:36:41 +0700

    feat(nvim): experiment with nvim-surround

--- configs/nvim/lua/plugin/editing/surround.lua
+++ configs/nvim/lua/plugin/editing/surround.lua
@@ -1,3 +1,14 @@
 local registry = require('lib.registry')
 
-registry.install('tpope/vim-surround')
+registry.install {
+  'tpope/vim-surround',
+  skip = registry.experiment('surround').on
+}
+
+registry.install {
+  'kylechui/nvim-surround',
+  skip = registry.experiment('surround').off,
+  config = function ()
+    require('nvim-surround').setup {}
+  end
+}
--- configs/nvim/lua/plugin/experiments.lua
+++ configs/nvim/lua/plugin/experiments.lua
@@ -4,3 +4,5 @@ local registry = require('lib.registry')
 registry.experiment('scroll', 'satellite')
 -- Experiment between fzf-lua and telescope
 registry.experiment('fuzzy', 'telescope')
+-- Experiment between vim-surround and nvim-surround
+registry.experiment('surround', false)
