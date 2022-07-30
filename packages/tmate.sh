3a8d1077c4ffa701f9588c9e4b361309011dedbf
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-07-30 15:27:58 +0700

    feat(packages): add tmate

---
+++ packages/tmate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if
+  ! (command -v force_print >/dev/null 2>&1) ||
+  ! (force_print 3 a b >/dev/null 2>&1) ||
+  test "$(force_print 3 a b)" != "a  b";
+then
+  printf "Please run this script through \"install.sh\" instead"
+  exit 1
+fi
+
+has_executable 'tmate'
+
+use_apk 'testing' 'tmate'
+use_apt 'tmate'
+use_pacman 'tmate'
+use_brew formula 'tmate'
