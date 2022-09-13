8dd634b4afb047fd593e7900d0531041d700fbbc
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-09-14 01:43:26 +0700

    refactor(nvim): now-playing no longer requires utf8 lib

--- configs/nvim/lua/plugin/appearance/now-playing.lua
+++ configs/nvim/lua/plugin/appearance/now-playing.lua
@@ -2,12 +2,6 @@ local registry = require('lib.registry')
 
 registry.install {
   'spywhere/now-playing.nvim',
-  requires = {
-    {
-      -- optional utf8 dependencies for better string manipulation
-      'uga-rosa/utf8.nvim',
-    }
-  },
   delay = function ()
     require('now-playing').setup {}
   end
