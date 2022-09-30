3f7ef3fa4aed54f6d94ca737748f26da6e526129
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-10-01 00:47:10 +0700

    refactor(tmux): better handling unavailable weather service

--- supports/tmux/scripts/weather.sh
+++ supports/tmux/scripts/weather.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-curl wttr.in/Bangkok?format=%f
+weather="$(curl --fail-early -m 2 -fsSL wttr.in/Bangkok?format=%f 2>/dev/null)"
+if test $? -eq 0; then
+  printf "%s" "$weather"
+fi
+printf ""
