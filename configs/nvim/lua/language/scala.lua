7eb2c35e5bf703d682d556028d99b1c3906b1b2d
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-10-12 15:05:19 +0700

    feat(nvim): metals progress report from status

--- configs/nvim/lua/language/scala.lua
+++ configs/nvim/lua/language/scala.lua
@@ -1,9 +1,31 @@
 local lsp = require('lib.lsp')
 
+local function status_handler(_, status, ctx)
+  local val = {}
+  if status.hide then
+    val = {kind = 'end'}
+  elseif status.show then
+    val = {kind = 'begin', message = status.text, title='metals'}
+  elseif status.text then
+    val = {kind = 'report', message = status.text, title='metals'}
+  else
+    return
+  end
+  local info = {client_id = ctx.client_id}
+  local msg = {token = 'metals', value = val}
+  -- call fidget progress handler
+  vim.lsp.handlers['$/progress'](nil, msg, info)
+end
+
 lsp.setup('metals')
   .filetypes({ 'scala', 'sbt', 'java' })
   .options(function ()
-    return require('metals').bare_config()
+    local config = require('metals').bare_config()
+
+    config.init_options.statusBarProvider = 'on'
+    config.handlers['metals/status'] = status_handler
+
+    return config
   end)
   .auto(function (config)
     config.settings = {
