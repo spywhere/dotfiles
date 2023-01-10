429d4947dac6fddb59251ff60fb52e07dfbc53df
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-01-10 16:08:32 +0700

    refactor(nvim): try out nvim-surround again

--- configs/nvim/lua/plugin/experiments.lua
+++ configs/nvim/lua/plugin/experiments.lua
@@ -1,4 +1,4 @@
 local registry = require('lib.registry')
 
 -- Experiment between vim-surround and nvim-surround
-registry.experiment('surround', false, 1667323179)
+registry.experiment('surround', true, 1673341664)
