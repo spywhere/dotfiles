936f8940a865c3da6e1da858c3e9e47387490785
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-04-10 17:10:31 +0700

    feat(nvim): pin bufferline.nvim

--- configs/nvim/lua/plugin/appearance/bufferline.lua
+++ configs/nvim/lua/plugin/appearance/bufferline.lua
@@ -3,6 +3,9 @@ local bindings = require('lib.bindings')
 
 registry.install {
   'akinsho/bufferline.nvim',
+  options = {
+    tag = 'v3.*'
+  },
   delay = function ()
     local is_gui = fn.exists('g:GuiLoaded') == 1
     require('bufferline').setup({
