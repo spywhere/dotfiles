c7504458510c58e2205acb88a54feca9a06b60ea
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-04-10 16:33:53 +0700

    feat(nvim): use nvim-surround

--- configs/nvim/lua/plugin/editing/surround.lua
+++ configs/nvim/lua/plugin/editing/surround.lua
@@ -1,13 +1,7 @@
 local registry = require('lib.registry')
 
 registry.install {
-  'tpope/vim-surround',
-  skip = registry.experiment('surround').on
-}
-
-registry.install {
   'kylechui/nvim-surround',
-  skip = registry.experiment('surround').off,
   config = function ()
     require('nvim-surround').setup {}
   end
--- configs/nvim/lua/plugin/experiments.lua
+++
@@ -1,4 +0,0 @@
-local registry = require('lib.registry')
-
--- Experiment between vim-surround and nvim-surround
-registry.experiment('surround', true, 1673341664)
