bf13505960686b7bb4a78332ab843e1c29414c78
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-06-18 02:08:46 +0700

    feat(nvim): use clippy for rust projects

--- configs/nvim/lua/language/rust.lua
+++ configs/nvim/lua/language/rust.lua
@@ -2,3 +2,12 @@ local lsp = require('lib.lsp')
 
 lsp.setup('rust_analyzer')
   .need_executable('rust-analyzer')
+  .options({
+    settings = {
+      ['rust-analyzer'] = {
+        check = {
+          command = 'clippy'
+        }
+      }
+    }
+  })
