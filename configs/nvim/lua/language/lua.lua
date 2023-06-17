bff690515a2262d3edf88368c43947c25d45751b
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-06-18 00:48:47 +0700

    feat(nvim): improve nvim lua support

--- configs/nvim/lua/language/lua.lua
+++ configs/nvim/lua/language/lua.lua
@@ -2,3 +2,21 @@ local lsp = require('lib.lsp')
 
 lsp.setup('lua_ls')
   .need_executable('lua-language-server')
+  .options({
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT'
+        },
+        diagnostics = {
+          globals = { 'vim' }
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true)
+        },
+        telemetry = {
+          enable = false
+        }
+      }
+    }
+  })
