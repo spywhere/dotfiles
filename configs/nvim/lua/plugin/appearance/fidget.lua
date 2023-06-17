c069a0d1b2f504c5bf396090ab32313f29d149b9
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-06-18 01:20:45 +0700

    refactor(nvim): use legacy fidget until rewritten complete

--- configs/nvim/lua/plugin/appearance/fidget.lua
+++ configs/nvim/lua/plugin/appearance/fidget.lua
@@ -2,6 +2,9 @@ local registry = require('lib.registry')
 
 registry.install {
   'j-hui/fidget.nvim',
+  options = {
+    tag = "legacy"
+  },
   lazy = true,
   config = function ()
     require('fidget').setup {
