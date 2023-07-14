2706fb4320382d4a63fcae83d7c68867f11ccb4a
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-07-14 18:37:34 +0700

    fix(nvim): ignore copilot on some filetypes

--- configs/nvim/lua/plugin/autocomplete/copilot.lua
+++ configs/nvim/lua/plugin/autocomplete/copilot.lua
@@ -7,5 +7,9 @@ registry.install {
     vim.g.copilot_no_tab_map = 1
     vim.g.copilot_assume_mapped = 1
     vim.g.copilot_tab_fallback = ''
+    vim.g.copilot_filetypes = {
+      TelescopePrompt = false,
+      DressingInput = false
+    }
   end
 }
