1e3bdf994ea739d563ad81a541470663c7348559
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-09-26 17:52:24 +0700

    refactor(nvim): use emmet_ls instead of ls_emmet

--- configs/nvim/lua/language/web.lua
+++ configs/nvim/lua/language/web.lua
@@ -4,18 +4,8 @@ local capabilities = lsp.capabilities({
   snippetSupport = true
 })
 
-lsp.setup('ls_emmet')
-  .need_executable('ls_emmet')
-  .config({
-    default_config = {
-      cmd = { 'ls_emmet', '--stdio' },
-      filetypes = { 'html', 'jsx', 'xml', 'css', 'scss', 'less' },
-      root_dir = function ()
-        return luv.cwd()
-      end,
-      settings = {}
-    }
-  })
+lsp.setup('emmet_ls')
+  .need_executable('emmet_ls')
   .options({
     capabilities = capabilities
   })
