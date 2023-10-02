e224af57bef80a60952ac2565e9aa48684420575
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-10-02 23:58:48 +0700

    feat(tmux): cache weather for 5 minutes

--- supports/tmux/scripts/weather.sh
+++ supports/tmux/scripts/weather.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-weather="$(curl --fail-early -m 2 -fsSL wttr.in/Bangkok?format=%f 2>/dev/null)"
+. "$HOME/.dots/binaries/_cache"
+
+_weather() {
+  curl --fail-early -m 2 -fsSL wttr.in/Bangkok?format=%f 2>/dev/null
+}
 if test $? -eq 0; then
-  printf "%s" "$weather"
+  printf "%s" "$(_cache_value weather _weather -t 300)"
 fi
 printf ""
