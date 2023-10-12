944950f2072057822cc00bcb4cae5b0ef2c69e36
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-10-12 15:09:30 +0700

    feat(nvim): remove editorconfig
    as it was already built-in on v0.9

--- configs/nvim/lua/plugin/standard/editorconfig.lua
+++
@@ -1,8 +0,0 @@
-local registry = require('lib.registry')
-
-registry.install {
-  'editorconfig/editorconfig-vim',
-  config = function ()
-    vim.g.Editorconfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
-  end
-}
