e047df8b917f02dddc28d3a566af5155fb0475f4
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-12-18 19:01:11 +0700

    fix(nvim): select ui is too big

--- configs/nvim/lua/plugin/appearance/dressing.lua
+++ configs/nvim/lua/plugin/appearance/dressing.lua
@@ -3,6 +3,18 @@ local registry = require('lib.registry')
 registry.install {
   'stevearc/dressing.nvim',
   config = function ()
-    require('dressing').setup {}
+    require('dressing').setup {
+      select = {
+        fzf_lua = {
+          winopts = {
+            height = 15,
+            width = 80
+          },
+          fzf_opts = {
+            ['--layout'] = 'reverse'
+          }
+        }
+      }
+    }
   end
 }
