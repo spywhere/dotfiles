01a9d672323bc588a801e460baad7b9d5602d1ff
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2024-01-26 17:25:40 +0700

    refactor(nvim): dap setup is now lazily called

--- configs/nvim/lua/lib/dap.lua
+++ configs/nvim/lua/lib/dap.lua
@@ -1,10 +1,11 @@
 -- NOTE: This require 'nvim-dap' to be installed
-local adapters = {}
-local dap_configs = {}
+local daps = {}
 
 local generate_dap_setup = function (name)
-  if not adapters[name] then
-    adapters[name] = {}
+  if not daps[name] then
+    daps[name] = {
+      options = {}
+    }
   end
 
   local DAPM = {}
@@ -17,75 +18,142 @@ local generate_dap_setup = function (name)
   end
 
   DAPM.adapter = wrap(function (adapter)
-    adapters[name] = adapter
+    table.insert(daps[name].options, adapter)
   end)
   DAPM.executable = wrap(function (name, args)
-    local name = name
-    local args = args
+    table.insert(daps[name].options, function ()
+      local name = name
+      local args = args
 
-    if type(name) == 'function' then
-      name = name()
-    end
+      if type(name) == 'function' then
+        name = name()
+      end
 
-    adapters[name] = {
-      type = 'executable',
-      name = name,
-      args = args
-    }
+      return {
+        type = 'executable',
+        name = name,
+        args = args
+      }
+    end)
   end)
   DAPM.server = wrap(function (host, port)
-    local host = host
-    local port = port
+    table.insert(daps[name].options, function ()
+      local host = host
+      local port = port
 
-    if type(host) == 'function' then
-      host, port = host()
-    end
+      if type(host) == 'function' then
+        host, port = host()
+      end
 
-    adapters[name] = {
-      type = 'server',
-      host = host,
-      port = port
-    }
+      return {
+        type = 'server',
+        host = host,
+        port = port
+      }
+    end)
   end)
 
   DAPM.ft = wrap(function (filetypes, ...)
     if type(filetypes) ~= 'table' then
       filetypes = { filetypes }
     end
-    for _, filetype in ipairs(filetypes) do
-      if not dap_configs[filetype] then
-        dap_configs[filetype] = {}
-      end
-      local configs = { ... }
-      for _, config in ipairs(configs) do
-        if type(config) == 'function' then
-          config = config()
-        end
-        config.type = name
-        table.insert(dap_configs[filetype], config)
-      end
+    if not daps[name].filetypes then
+      daps[name].filetypes = {}
     end
+    local configs = { ... }
+    daps[name].filetypes = filetypes
+    daps[name].configs = configs
   end)
 
   return DAPM
 end
 
+local setup_dap = function (name, adapter, setter)
+  local dap_setter = {
+    adapter = function (dap_name, dap_adapter)
+      local dap = require('dap')
+      dap.adapters[dap_name] = dap_adapter
+    end,
+    config = function (filetype, config)
+      local dap = require('dap')
+      if dap.configurations[filetype] then
+        table.insert(dap.configurations[filetype], config)
+      else
+        dap.configurations[filetype] = { config }
+      end
+    end
+  }
+
+  if not setter then
+    setter = dap_setter
+  end
+  if not setter.adapter then
+    setter.adapter = dap_setter.adapter
+  end
+  if not setter.config then
+    setter.config = dap_setter.config
+  end
+
+  local dap_adapter = {}
+
+  for _, option in ipairs(adapter.options) do
+    if type(option) == 'function' then
+      option = option()
+    end
+    dap_adapter = vim.tbl_extend('force', dap_adapter, option)
+  end
+
+  setter.adapter(name, dap_adapter)
+
+  if adapter.configs then
+    for _, filetype in ipairs(adapter.filetypes) do
+      for _, config in ipairs(adapter.configs) do
+        if type(config) == 'function' then
+          config = config()
+        end
+        config.type = name
+        setter.config(filetype, config)
+      end
+    end
+  end
+end
+
 local M = {}
 local has_been_setup = false
 
+local function setup(setup_fn)
+  if has_been_setup then
+    return
+  end
+  assert(not has_been_setup, 'DAP setup has already been called')
+
+  setup_fn()
+
+  has_been_setup = true
+end
+
 M.setup = function (name)
-  if name == nil then
-    assert(not has_been_setup, 'DAP setup has already been called')
-
-    local dap = require('dap')
-
-    for name, adapter in pairs(adapters) do
-      dap.adapters[name] = adapter
+  if type(name) == 'function' then
+    -- return a setup function with a handler
+    return function ()
+      local function handler(dap_name, setter)
+        if daps[dap_name] then
+          setup_dap(dap_name, daps[dap_name], setter)
+        end
+      end
+      return setup(function ()
+        name(handler)
+      end)
     end
-    for filetype, configs in pairs(dap_configs) do
-      dap.configurations[filetype] = configs
-    end
+  elseif name == nil then
+    return setup(function ()
+      -- perform a setup for DAPs
+      for dap_name, adapter in pairs(daps) do
+        setup_dap(dap_name, adapter)
+      end
+    end)
   else
+    -- generate a chainable object for setting up DAP
     return generate_dap_setup(name)
   end
 end
