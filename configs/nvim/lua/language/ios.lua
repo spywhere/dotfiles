c7c86396784d9e1eb5b3fa692d654afac4ee5ae8
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2024-01-31 13:30:19 +0700

    refactor(nvim): use xcode lsp when possible

--- configs/nvim/lua/language/ios.lua
+++ configs/nvim/lua/language/ios.lua
@@ -3,6 +3,30 @@ local dap = require('lib.dap')
 
 lsp.setup('sourcekit')
   .need_executable('sourcekit-lsp')
+  .command(function ()
+    local command = 'sourcekit-lsp'
+    if fn.executable('xcode-select') == 0 then
+      return command
+    end
+
+    local dev_dir = fn.trim(fn.system('xcode-select -p'))
+    local toolchain_dir = dev_dir .. '/Toolchains/XcodeDefault.xctoolchain'
+    if fn.isdirectory(toolchain_dir) == 0 then
+      return command
+    end
+
+    local bin_dir = toolchain_dir .. '/usr/bin'
+    if fn.isdirectory(bin_dir) == 0 then
+      return command
+    end
+
+    local lsp_bin = bin_dir .. '/sourcekit-lsp'
+    if fn.executable(lsp_bin) == 0 then
+      return command
+    else
+      return { lsp_bin }
+    end
+  end)
   .root(function (filename)
     local util = require('lspconfig.util')
     return util.root_pattern('buildServer.json')(filename)
