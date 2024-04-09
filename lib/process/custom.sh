cbe609a523efea838b59c50824c168c1e544a1e0
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2024-04-09 17:09:34 +0700

    refactor(install): clean up conditions and remove manual installs

--- lib/process/custom.sh
+++ lib/process/custom.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
 _summarize_custom() {
-  if test -n "$_CUSTOM"; then
-    print "$esc_green==>$esc_reset The following installations will be run:"
-    eval "set -- $_CUSTOM"
-    for try_run_install__fn in "$@"; do
-      try_run_install__package_name="$(parse_field "$try_run_install__fn" package_name)"
-
-      if test -z "$try_run_install__package_name"; then
-        try_run_install__package_name="$(parse_field "$try_run_install__fn" fn)"
-      fi
-
-      print "  $esc_blue-$esc_reset $try_run_install__package_name"
-    done
+  if test -z "$_CUSTOM"; then
+    return
   fi
+
+  print "$esc_green==>$esc_reset The following installations will be run:"
+  eval "set -- $_CUSTOM"
+  for try_run_install__fn in "$@"; do
+    try_run_install__package_name="$(parse_field "$try_run_install__fn" package_name)"
+
+    if test -z "$try_run_install__package_name"; then
+      try_run_install__package_name="$(parse_field "$try_run_install__fn" fn)"
+    fi
+
+    print "  $esc_blue-$esc_reset $try_run_install__package_name"
+  done
 }
 
 _run_custom() {
-  if test -n "$_CUSTOM"; then
-    step "Performing custom installations..."
-    eval "set -- $_CUSTOM"
-    for try_run_install__custom in "$@"; do
-      try_run_install__fn="$(parse_field "$try_run_install__custom" fn)"
-      "$try_run_install__fn"
-    done
+  if test -z "$_CUSTOM"; then
+    return
   fi
+
+  step "Performing custom installations..."
+  eval "set -- $_CUSTOM"
+  for try_run_install__custom in "$@"; do
+    try_run_install__fn="$(parse_field "$try_run_install__custom" fn)"
+    "$try_run_install__fn"
+  done
 }
--- lib/process/setup.sh
+++ lib/process/setup.sh
@@ -1,48 +1,59 @@
 #!/bin/sh
 
 _prepare_setup() {
-  if ! _has_skip setup || test -n "$_INDICATED"; then
-    for try_run_install__setup_path in "$HOME/$DOTFILES/setup"/*.sh; do
-      try_run_install__setup=$(basename "$try_run_install__setup_path")
-      try_run_install__setup=${try_run_install__setup%.sh}
-
-      print_inline "$esc_yellow==>$esc_reset Checking setup $try_run_install__setup..."
-
-      # Skip requested setups
-      if (_has_skip setup || _has_skip "$try_run_install__setup") && ! _has_indicate "$try_run_install__setup"; then
-        continue
-      fi
-
-      _RUNNING="$try_run_install__setup"
-      _FULFILLED=""
-      . "$try_run_install__setup_path"
-    done
+  if ! test -d "$HOME/$DOTFILES/setup"; then
+    warn "No setup found"
+    return
   fi
+
+  if _has_skip setup && test -z "$_INDICATED"; then
+    return
+  fi
+
+  for try_run_install__setup_path in "$HOME/$DOTFILES/setup"/*.sh; do
+    try_run_install__setup=$(basename "$try_run_install__setup_path")
+    try_run_install__setup=${try_run_install__setup%.sh}
+
+    print_inline "$esc_yellow==>$esc_reset Checking setup $try_run_install__setup..."
+
+    # Skip requested setups
+    if (_has_skip setup || _has_skip "$try_run_install__setup") && ! _has_indicate "$try_run_install__setup"; then
+      continue
+    fi
+
+    _RUNNING="$try_run_install__setup"
+    _FULFILLED=""
+    . "$try_run_install__setup_path"
+  done
 }
 
 _summarize_setup() {
-  if test -n "$_SETUP"; then
-    print "$esc_green==>$esc_reset The following setups will be run:"
-    eval "set -- $_SETUP"
-    for try_run_install__fn in "$@"; do
-      try_run_install__setup_name="$(parse_field "$try_run_install__fn" display_name)"
-
-      if test -z "$try_run_install__setup_name"; then
-        try_run_install__setup_name="$(parse_field "$try_run_install__fn" fn)"
-      fi
-
-      print "  $esc_blue-$esc_reset $try_run_install__setup_name"
-    done
+  if test -z "$_SETUP"; then
+    return
   fi
+
+  print "$esc_green==>$esc_reset The following setups will be run:"
+  eval "set -- $_SETUP"
+  for try_run_install__fn in "$@"; do
+    try_run_install__setup_name="$(parse_field "$try_run_install__fn" display_name)"
+
+    if test -z "$try_run_install__setup_name"; then
+      try_run_install__setup_name="$(parse_field "$try_run_install__fn" fn)"
+    fi
+
+    print "  $esc_blue-$esc_reset $try_run_install__setup_name"
+  done
 }
 
 _run_setup() {
-  if test -n "$_SETUP"; then
-    step "Running setups..."
-    eval "set -- $_SETUP"
-    for try_run_install__setup in "$@"; do
-      try_run_install__fn="$(parse_field "$try_run_install__setup" fn)"
-      "$try_run_install__fn"
-    done
+  if test -z "$_SETUP"; then
+    return
   fi
+
+  step "Running setups..."
+  eval "set -- $_SETUP"
+  for try_run_install__setup in "$@"; do
+    try_run_install__fn="$(parse_field "$try_run_install__setup" fn)"
+    "$try_run_install__fn"
+  done
 }
