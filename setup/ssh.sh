e997557f20635aee3ab633c49f1a570641a1fd86
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2024-04-22 20:07:09 +0700

    feat(ssh): use security key for ssh authentication

--- setup/ssh.sh
+++ setup/ssh.sh
@@ -13,32 +13,115 @@ fi
 
 add_setup 'setup_ssh'
 
-try_generate_keypair__has_generate=0
-try_generate_keypair() {
-  try_generate_keypair__path="$HOME/$DOTFILES/configs/ssh/$1.id_rsa"
-  if test -f "$try_generate_keypair__path"; then
-    info "SSH keypair for $1 is already exists"
-  else
-    step "Generating SSH keypair for $1..."
-    ssh-keygen -b 2048 -t rsa -f "$try_generate_keypair__path" -q -N ""
+ssh__target="$HOME/$DOTFILES/configs/ssh"
 
-    if test "$try_generate_keypair__has_generate" -eq 0; then
-        add_post_install_message "Revoke and reassign a new SSH keypair"
-        try_generate_keypair__has_generate=1
-    fi
+try_generate_keypairs__has_generate=0
+try_generate_keypairs() {
+  if _has_indicate ssh-rsa; then
+    try_generate_keypairs__suffix="id_rsa"
+    try_generate_keypairs__generate() {
+      ssh-keygen -b 2048 -t rsa -f "$1" -q -N ""
+    }
+  else
+    try_generate_keypairs__suffix="sk"
+    try_generate_keypairs__user="$(whoami)"
+    try_generate_keypairs__generate() {
+      ssh-keygen -t ed25519-sk -f "$1" -O resident -O application="ssh://$2" -O user="$try_generate_keypairs__user" -O verify-required -N ""
+    }
   fi
+
+  while test -n "$1"; do
+    try_generate_keypairs__path="$ssh__target/$1.$try_generate_keypairs__suffix"
+
+    if test -f "$try_generate_keypairs__path"; then
+      info "SSH keypair for $1 is already exists"
+      shift
+      continue
+    fi
+
+    step "Generating SSH keypair for $1..."
+    try_generate_keypairs__generate "$try_generate_keypairs__path" "$1"
+
+    if test "$try_generate_keypairs__has_generate" -eq 0; then
+        add_post_install_message "Revoke and reassign a new SSH keypair"
+        try_generate_keypairs__has_generate=1
+    fi
+    shift
+  done
+}
+
+contains() {
+  case "$1" in
+    *"$2"*)
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
+}
+
+has_suffix() {
+  case "$1" in
+    *"$2")
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
+}
+
+lookup_keypairs() {
+  lookup_keypairs__path="$1"
+  shift
+
+  while test -n "$1"; do
+    if ! contains  "$lookup_keypairs__path" "$1"; then
+      shift
+      continue
+    fi
+
+    printf '%s' "$1"
+    break
+  done
+}
+
+download_keypairs() {
+  download_keypairs__path="$(deps keys)"
+  if ! test -d "$download_keypairs__path"; then
+    mkdir -p "$download_keypairs__path"
+  fi
+  cmd cd "$download_keypairs__path"
+
+  ssh-keygen -K -q -P ''
+  for file in ./id_*; do
+    # shellcheck disable=SC2068
+    download_keypairs__name="$(lookup_keypairs "$file" $@)"
+    if has_suffix "$file" ".pub"; then
+      download_keypairs__file="$download_keypairs__name.sk.pub"
+    else
+      download_keypairs__file="$download_keypairs__name.sk"
+    fi
+    download_keypairs__path="$ssh__target/$download_keypairs__file"
+
+    if test -f "$download_keypairs__path"; then
+      info "SSH keypair for $download_keypairs__file is already exists"
+      continue
+    fi
+
+    info "Download key $download_keypairs__file to $download_keypairs__path"
+    cmd mv "$file" "$download_keypairs__path"
+  done
+
+  cmd cd "$CURRENT_DIR"
 }
 
 setup_ssh() {
-  # generate SourceHut key pair
-  try_generate_keypair srht
+  if ! _has_indicate generate; then
+    download_keypairs srht github gitlab digitalocean
+    return
+  fi
 
-  # generate GitHub key pair
-  try_generate_keypair github
-
-  # generate GitLab key pair
-  try_generate_keypair gitlab
-
-  # generate Digital Ocean key pair
-  try_generate_keypair digitalocean
+  try_generate_keypairs srht github gitlab digitalocean
 }
