894c268178f292c078cdea56cf3b8cecebd47f33
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2024-08-16 00:02:43 +0700

    chore: enforce scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,15 @@ module.exports = {
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
     'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', [
+      'asdf',
+      'binaries',
+      'git',
+      'nvim',
+      'packages',
+      'setup',
+      'tmux',
+      'zsh',
+    ]],
   }
 };
