310b4d848e568058deb4669f9a0c4f1014fda8be
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2025-08-17 22:37:24 +0700

    refactor(nvim): update tmux-navigator keymap

--- configs/nvim/lua/editor/terminal.lua
+++ configs/nvim/lua/editor/terminal.lua
@@ -2,7 +2,7 @@ local bindings = require('lib.bindings')
 local registry = require('lib.registry')
 
 local quick_terminal = function ()
-  bindings.map.normal('<leader>t', '<cmd>25split | terminal<cr>')
+  bindings.map.normal('<leader>t', '<cmd>vsplit | terminal<cr>')
 end
 registry.defer_first(quick_terminal)
 
--- configs/nvim/lua/plugin/navigation/tmux-navigator.lua
+++ configs/nvim/lua/plugin/navigation/tmux-navigator.lua
@@ -1,6 +1,16 @@
 local registry = require('lib.registry')
+local bindings = require('lib.bindings')
 
 registry.install {
   'christoomey/vim-tmux-navigator',
-  lazy = true
+  lazy = true,
+  setup = function ()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
+  config = function ()
+    bindings.map.normal('<C-h>', ':<C-U>TmuxNavigateLeft<cr>')
+    bindings.map.normal('<C-j>', ':<C-U>TmuxNavigateDown<cr>')
+    bindings.map.normal('<C-k>', ':<C-U>TmuxNavigateUp<cr>')
+    bindings.map.normal('<C-l>', ':<C-U>TmuxNavigateRight<cr>')
+  end
 }
