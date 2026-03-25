e2b1cb3b31bf24181eb8187e69257f0c3d8c5dba
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-03-25 21:27:50 +0700

    refactor(configs): reformat sketchybar items

--- configs/sketchybar/items/apple.sh
+++ configs/sketchybar/items/apple.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sketchybar --add item apple left \
-           --set apple icon=󰀵 icon.font="SF Pro:Regular:18" icon.padding_left=0
+           --set apple \
+           icon=󰀵 \
+           icon.font="SF Pro:Regular:18" \
+           icon.padding_left=0
--- configs/sketchybar/items/clock.sh
+++ configs/sketchybar/items/clock.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 sketchybar --add item clock right \
-           --set clock update_freq=1 icon=  script="$CONFIG_DIR/plugins/clock.sh"
+           --set clock \
+           update_freq=1 \
+           icon=  \
+           label.padding_right=0 \
+           script="$CONFIG_DIR/plugins/clock.sh"
