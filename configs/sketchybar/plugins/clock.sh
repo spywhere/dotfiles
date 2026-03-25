310dd508a120a97640fd811619a4120ba4bdf850
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-03-25 21:24:15 +0700

    feat(configs): blinking clock item

--- configs/sketchybar/plugins/clock.sh
+++ configs/sketchybar/plugins/clock.sh
@@ -4,4 +4,8 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%a %d %b  %H:%M')"
+clock_separator=" "
+if test "$(( $(date '+%S') % 2 ))" -eq 0; then
+  clock_separator=":"
+fi
+sketchybar --set "$NAME" label="$(date "+%a %d %b  %H$clock_separator%M")"
