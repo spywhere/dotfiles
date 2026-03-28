f326dff79a9e5db44c9891ca08e1e41236b9e46f
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-03-29 03:28:39 +0700

    feat(sketchybar): color coded menu item on battery charging

--- configs/sketchybar/plugins/battery.sh
+++ configs/sketchybar/plugins/battery.sh
@@ -32,6 +32,7 @@ esac
 
 if [[ "$CHARGING" != "" ]]; then
   ICON="􀢋"
+  LABEL_COLOR=0xff00aaff
 fi
 
 if test -z "$REMAINING"; then
@@ -41,17 +42,19 @@ if test -z "$REMAINING"; then
     --set "$NAME" \
     label.y_offset=0 \
     label.font.size=13 \
-    label.width=35
+    label.width=35 \
+    label.color="$LABEL_COLOR"
 else
   sketchybar --animate sin 10 \
     --set "$NAME.status" \
-    label.color=0xffffffff \
+    label.color="$LABEL_COLOR" \
     --set "$NAME" \
     label.y_offset=-5 \
     label.font.size=8 \
-    label.width=30
+    label.width=30 \
+    label.color="$LABEL_COLOR"
 
-  sketchybar --set "$NAME.status" label="$REMAINING" label.color="$LABEL_COLOR"
+  sketchybar --set "$NAME.status" label="$REMAINING"
 fi
 
-sketchybar --set "$NAME" icon="$ICON" label="$PERCENTAGE%" label.color="$LABEL_COLOR"
+sketchybar --set "$NAME" icon="$ICON" label="$PERCENTAGE%"
