4f4749ba7a936f9478aa22117b48aa4be8f7df97
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-03-31 17:11:33 +0700

    feat(sketchybar): add app badge for proton mail

--- configs/sketchybar/items/app_badge.sh
+++ configs/sketchybar/items/app_badge.sh
@@ -10,6 +10,16 @@ sketchybar --add item slack right \
            script="$CONFIG_DIR/plugins/app_badge.sh Slack 0xffeed49f 0xffed8796" \
            --subscribe slack front_app_switched
 
+sketchybar --add item protonmail right \
+           --set protonmail \
+           drawing=off \
+           icon="􀍛" \
+           icon.font.size=18 \
+           icon.padding_right=0 \
+           update_freq=60 \
+           script="$CONFIG_DIR/plugins/app_badge.sh 'Proton Mail' 0xff99ccff" \
+           --subscribe protonmail front_app_switched
+
 sketchybar --add item outlook right \
            --set outlook \
            drawing=off \
