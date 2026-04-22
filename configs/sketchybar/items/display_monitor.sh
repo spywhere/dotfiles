74df95f102a0df9c35fbd8178604426056ffab61
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-04-22 15:49:44 +0700

    fix(sketchybar): display monitor is not working again and add icon

--- configs/sketchybar/items/display_monitor.sh
+++ configs/sketchybar/items/display_monitor.sh
@@ -2,7 +2,10 @@
 
 sketchybar --add item display_monitor right \
   --set display_monitor \
-  drawing=off \
+  icon.drawing=off \
+  icon=􀨧 \
+  label.drawing=off \
   label="$(sketchybar --query displays | sha512)" \
   script="$CONFIG_DIR/plugins/display_monitor.sh" \
+  update_freq=5 \
   --subscribe display_monitor display_change
--- configs/sketchybar/plugins/display_monitor.sh
+++ configs/sketchybar/plugins/display_monitor.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-current_displays="$(sketchybar --query "$NAME" | jq -r .label.value)"
-updated_displays="$(sketchybar --query displays | sha512)"
+case "$SENDER" in
+  display_change)
+    current_displays="$(sketchybar --query "$NAME" | jq -r .label.value)"
+    updated_displays="$(sketchybar --query displays | sha512)"
 
-if test "$current_displays" != "$updated_displays"; then
-  bash "$(echo "$CONFIG_DIR" | dirname)/aerospace/reorganize-workspaces.sh"
-  sketchybar --set "$NAME" label="$updated_displays"
-fi
+    if test "$current_displays" != "$updated_displays"; then
+      bash "$(dirname "$CONFIG_DIR")/aerospace/reorganize-workspaces.sh"
+      sketchybar --set "$NAME" label="$updated_displays" icon.drawing=on
+    fi
+    ;;
+  forced)
+    ;;
+  *)
+    sketchybar --set "$NAME" icon.drawing=off
+    ;;
+esac
