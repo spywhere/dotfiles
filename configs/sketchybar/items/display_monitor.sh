ae56227860156dc3666d688e7f4a8b229b5941a1
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-04-22 14:26:03 +0700

    fix(sketchybar): display monitor is not working

--- configs/sketchybar/items/display_monitor.sh
+++ configs/sketchybar/items/display_monitor.sh
@@ -4,5 +4,5 @@ sketchybar --add item display_monitor right \
   --set display_monitor \
   drawing=off \
   label="$(sketchybar --query displays | sha512)" \
-  script="$CONFIG_DIR/plugins/display_change.sh" \
+  script="$CONFIG_DIR/plugins/display_monitor.sh" \
   --subscribe display_monitor display_change
