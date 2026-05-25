59ec75804c6f40b0e3594aa7638e7c00ebeb3151
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-05-25 20:15:23 +0700

    refactor(sketchybar): update raycast menu items

--- configs/sketchybar/items/events.sh
+++ configs/sketchybar/items/events.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sketchybar --add alias "Control Center,raycastCalendarStatusItem" right
+# sketchybar --add alias "Control Center,raycastCalendarStatusItem" right
+sketchybar --add alias "Control Center,calendar-events" right
