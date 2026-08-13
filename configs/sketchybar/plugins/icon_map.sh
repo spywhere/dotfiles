6cdee937d914db66fc80a28e60b2644d5ccd5e38
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-08-13 21:17:09 +0700

    feat(sketchybar): use custom icon for dogfooding

--- configs/sketchybar/plugins/icon_map.sh
+++ configs/sketchybar/plugins/icon_map.sh
@@ -2,4 +2,9 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 
+if test "$1" = "Voyager" -o "$1" = "Voyager Beta"; then
+  echo ":tabby:"
+  exit
+fi
+
 jq -r --arg app "$1" 'map(select(.appNames|index([$app])))|first|.iconName//":default:"' "$SCRIPT_DIR/icon_map.json"
