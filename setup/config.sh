857a3c45e3a77912fc5dd139153fc2edefb588e6
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2026-08-15 23:16:45 +0700

    feat(herdr): add herdr-reviewr configs

--- setup/config.sh
+++ setup/config.sh
@@ -338,6 +338,7 @@ setup_config() {
   fi
   link herdr/plugins/config/herdr-lazy/ .config/herdr/plugins/config/herdr-lazy
   link herdr/plugins/config/cloudmanic.herdr-plus/ .config/herdr/plugins/config/cloudmanic.herdr-plus
+  link herdr/plugins/config/persiyanov.reviewr/ .config/herdr/plugins/config/persiyanov.reviewr
   link herdr/plugins/config/spywhere.now-playing/ .config/herdr/plugins/config/spywhere.now-playing
 
   step "  - htop"
